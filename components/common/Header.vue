e874b493faa1d9d3e0d59d13234de7ae0572fe6f
ZhangShuai <ZhangS@EDZdeMacBook-Pro.local>
2018-05-25 16:30:06 +0800

    update

--- components/common/Header.vue
+++ components/common/Header.vue
@@ -10,22 +10,132 @@
       </div>
       <div class="HREntry">
         <span>Hr入口</span>
-        <img src="../../assets/images/phone.png" alt="">
+        <i class="phone" :style="bgi"></i>
       </div>
       <div class="lrBtn">
-        <span class="login">登录</span>
-        <span class="registe">注册</span>
+        <span class="login" @click="login">登录</span>
+        <span class="register">注册</span>
       </div>
-
     </div>
+
+    <!-- 登录注册 -->
+    <div class="start" v-show="start">
+      <div class="bgt" @click="close"></div>
+      <div class="lrFrame">
+        <el-tabs v-model="activeName" @tab-click="handleClick">
+          <el-tab-pane label="登录" name="login">
+            <!-- 登录 -->
+            <el-form :model="loginData" status-icon :rules="rules2" ref="loginData" class="demo-ruleForm">
+              <el-form-item prop="age">
+                <el-input v-model.number="loginData.tel" placeholder="请输入登录手机号"></el-input>
+              </el-form-item>
+              <el-form-item prop="pass">
+                <el-input :type="loginData.pwdType" v-model="loginData.pass" auto-complete="off" placeholder="6-10位密码，区分大小写，不能用空格"></el-input>
+                <span :class="{hidePwd:!showPwd,showPwd:showPwd}" @click="changePwd" alt=""></span>
+              </el-form-item>
+              <el-row>
+                <div>忘记密码?</div>
+                <el-button>登录</el-button>
+              </el-row>
+            </el-form>
+            <div class="otherLogin">其它方式登录</div>
+          </el-tab-pane>
+          <el-tab-pane label="注册" name="register">
+            <!-- 注册 -->
+            <el-form :model="loginData" status-icon :rules="rules2" ref="loginData" class="demo-ruleForm">
+              <el-form-item prop="age">
+                <el-input v-model.number="loginData.tel" placeholder="请输入登录手机号"></el-input>
+              </el-form-item>
+              <el-form-item prop="pass">
+                <el-input :type="loginData.pwdType" v-model="loginData.pass" auto-complete="off" placeholder="6-10位密码，区分大小写，不能用空格"></el-input>
+                <span :class="{hidePwd:!showPwd,showPwd:showPwd}" @click="changePwd" alt=""></span>
+              </el-form-item>
+              <el-row>
+                <div>忘记密码?</div>
+                <el-button>登录</el-button>
+              </el-row>
+            </el-form>
+            <div class="otherLogin">其它方式登录</div>
+          </el-tab-pane>
+        </el-tabs>
+        <div class="form">
+          
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
   export default {
+    methods:{
+      login(){
+        this.start=true;
+        this.activeName = 'login';
+      },
+      changePwd(){
+        console.log(this.showPwd);
+        if(this.showPwd){
+          this.showPwd = false;
+          this.loginData.pwdType = 'password';
+        }else{
+          this.showPwd = true;
+          this.loginData.pwdType = 'text';
+        }
+      },
+      register(){
+        this.start=true;
+        this.activeName = 'register';
+      },
+      close(){
+        this.start=false;
+      },
+      handleClick(){
+        
+      }
+    },
     data(){
+      var checkTel = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('手机号不能为空'));
+        }
+        setTimeout(() => {
+          if (!Number.isInteger(value)) {
+            callback(new Error('请输入正确手机号'));
+          }
+        }, 1000);
+      };
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+          return false;
+        } else if(value.length<6 || value.length>10){
+          callback(new Error('请输入6-10位密码'));
+          return false;
+        }
+      };
       return{
-        search:''
+        start:false,
+        search:'',
+        activeName: '',
+        showPwd: false,
+        pwdType:'password',
+        bgi:{
+          backgroundImage: "url(" + require("../../assets/images/phone.png") + ")",
+        },
+        loginData:{
+          pass:'',
+          tel: ''
+        },
+        rules2:{
+          pass: [
+            { validator: validatePass, trigger: 'blur' }
+          ],
+          tel: [
+            { validator: checkTel, trigger: 'blur' }
+          ]
+        }
       }
     }
   }
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template>
+  <div class="container">
     <div class="banner">
       <!-- banner -->
       <el-carousel :interval="5000" arrow="never">
@@ -31,18 +32,46 @@
           </el-tab-pane>
         </el-tabs>
       </div>
-
       <!-- 定制 -->
       <div class="customization">
         <div class="pro clearfix" v-for="(pro,index) in projects" :key="index">
           <img :src="pro.src" alt="" class="fl">
-          <div class="fr">
+          <div class="fl">
             <h5>{{pro.title}}</h5>
             <p>{{pro.content}}</p>
           </div>
         </div>
       </div>
     </div>
+
+    <!-- 名师大咖秀 -->
+    <div class="teachers">
+      <div class="blueBar"></div>
+      <div class="cardList">
+        <div class="card" v-for="(teacher,index) in teachers" :key="index">
+          <img :src="teacher.src" class="image">
+          <div class="diaphaneity">
+            <div class="octImg"></div>
+            <div class="abstract">
+              <h4>{{teacher.name}}</h4>
+              <p>{{teacher.abstract}}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- 合作伙伴 -->
+    <div class="partner">
+      <h4>合作伙伴</h4>
+      <p>50+国内外知名企业使用1911课堂</p>
+      <div class="partnerList">
+        <span v-for="(img,index) in partnerList" :key="index">
+          <img :src="img.src" alt="">
+        </span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -101,6 +130,26 @@ export default{
             title:'高效团队与卓越执行力 高效团队',
             author:'王建林 清华大学电子工程系教授'
           }
+        ],
+        teachers:[
+          {
+            src:require('../assets/images/teacher1.png'),
+            name:'张邵阳',
+            abstract:'源自清华、面向世界，以传播清华文化、服务创新创业、推动健康品质生活为己任，旗下布局四大业务板块：1911创客空间、1911资本、1911商城及1911教育。'
+          },
+          {
+            src:require('../assets/images/teacher2.png'),
+            name:'张邵阳',
+            abstract:'源自清华、面向世界，以传播清华文化、服务创新创业、推动健康品质生活为己任，旗下布局四大业务板块：1911创客空间、1911资本、1911商城及1911教育。'
+          },
+          {
+            src:require('../assets/images/teacher3.png'),
+            name:'张邵阳',
+            abstract:'源自清华、面向世界，以传播清华文化、服务创新创业、推动健康品质生活为己任，旗下布局四大业务板块：1911创客空间、1911资本、1911商城及1911教育。'
+          }
+        ],
+        partnerList:[
+          {src:require('../assets/images/ptn1.png'),},{src:require('../assets/images/ptn2.png'),},{src:require('../assets/images/ptn3.png'),},{src:require('../assets/images/ptn4.png'),},{src:require('../assets/images/ptn5.png'),},{src:require('../assets/images/ptn6.png'),},{src:require('../assets/images/ptn7.png'),},{src:require('../assets/images/ptn8.png'),},{src:require('../assets/images/ptn9.png'),},{src:require('../assets/images/ptn10.png'),}
         ]
       }
     }
