421e68bb779e2fa415863ae97447d7ceb354222a
wuyuyu <wuyuyu@1911Thu.com>
2018-06-11 01:39:29 +0800

    新闻资讯的样式

--- components/common/Pagination.vue
+++ components/common/Pagination.vue
@@ -1,30 +1,45 @@
 <template>
   <div class="center">
     <div class="pagination">
-      <el-pagination background layout="prev, pager, next" :page-size="2" :page-count="data.pagesize" :current-page="data.page" @current-change="handleCurrentChange" :total="data.total"></el-pagination>
+      <el-pagination background layout="prev, pager, next" :page-size="pageSize" :page-count="data.pagesize" :current-page="data.page" @current-change="handleCurrentChange" :total="data.total"></el-pagination>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    props: ["data"],
-    methods: {
-      handleCurrentChange(val) {
-        // console.log(val, '这是val')
-        this.$emit("page", val);
+export default {
+  props: {
+    data: {
+      type: Object,
+      default: function() {
+        return {
+          pagesize: 0,
+          page: 0,
+          total: 0
+        };
       }
+    },
+    pageSize: {
+      type: Number,
+      default: 2
     }
-  };
+  },
+  methods: {
+    handleCurrentChange(val) {
+      // console.log(val, '这是val')
+      this.$emit("page", val);
+    }
+  }
+};
 </script>
 
 <style scoped lang="scss">
-  .center {
-    width: 1100px;
-    margin: 0 auto;
-    .pagination {
-      text-align: center;
-      margin: 77px 0px 127px 0px;
-    }
+.center {
+  width: 1100px;
+  margin: 0 auto;
+  .pagination {
+    text-align: center;
+    margin: 77px 0px 127px 0px;
   }
+}
 </style>
