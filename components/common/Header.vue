afd50290e3bca3e323d086b31f01440fbea629c0
wuyuyu <wuyuyu@1911Thu.com>
2018-06-14 11:41:34 +0800

    (#158)个人中心点击我的课程进去后闪烁的问题

--- components/common/Header.vue
+++ components/common/Header.vue
@@ -24,7 +24,7 @@
         </div>
         <div class="shoppingCart" v-show="isAuthenticated" @click="goSearchd('/shop/shoppingCart')">
           <span class="cartIcon"></span>
-          <i>{{productsNum}}</i>
+          <i v-if="productsNum>0">{{productsNum}}</i>
         </div>
       </div>
       <div class="lrBtn" v-if="!isAuthenticated">
