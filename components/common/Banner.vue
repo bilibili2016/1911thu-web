c4586e19682de5205d1cfe9be68f9755787442d1
wuyuyu <wuyuyu@1911Thu.com>
2018-06-14 16:57:29 +0800

    修改图像的样式问题

--- components/common/Banner.vue
+++ components/common/Banner.vue
@@ -5,33 +5,21 @@
         <img :src="bannerImg" alt="">
       </div>
     </template>
-
     <template v-if="config.banner_type === 'profile'">
       <div class="profile-banner">
         <div class="center-box">
           <div class="avator">
             <div class="img">
-              <img :src="avator" alt="" />
-              <div class="up-user-avtor">
+              <img :src="avator" />
+              <div class="up-user-avtor" v-show="isShowUpAvtor">
                 <input type="file" @change="add_img" accept="image/png,image/gif,image/jpeg" />
                 <span>更换图片</span>
               </div>
-              <!-- <img :src="avator" alt="" v-if="userInfo.head_img"> -->
-              <!-- <img :src="userInfo.head_img" alt="" v-if="userInfo.head_img">
-                <img :src="avator" alt="" v-else> -->
-              <!-- <el-upload v-show="isShowUpAvtor" class="up-user-avtor"
-                  action="http://www.1911edu.com/Wapi/MyInfo/uploadHeadImg"
-                  accept='image/*'
-                  :on-success="upSuccess"
-                  :on-error="failUp"
-                  :show-file-list="false">
-                  <el-button size="medium" type="primary">更换图片</el-button>
-                </el-upload> -->
             </div>
           </div>
           <div class="name">
             <p>{{userInfo.nick_name}}</p>
-            <p>{{userInfo.company_name}}</p>
+            <p>{{userInfo.company_name}}{{avator}}</p>
           </div>
           <div class="time">
             <p class="fr">18小时32分钟</p>
@@ -49,7 +37,7 @@ export default {
   props: ['bannerImg', 'config', 'isUpdate', 'isShowUpAvtor'],
   data() {
     return {
-      avator: require('@/assets/images/profile_avator01.png'),
+      avator: '',
       userInfo: {
         nick_name: '',
         company_name: '',
@@ -83,34 +71,21 @@ export default {
       reader.onloadend = function() {
         that.fileForm.FILESS.push(reader.result)
         home.uploadHeadImg(that.fileForm).then(response => {
-          console.log(response)
+          this.avator = response.data.full_path
+          console.log(this.avator)
         })
       }
-    },
-    uploadImg(e) {
-      let param = new FormData() //创建form对象
-      param.append('file', e.target.files[0]) //通过append向form对象添加数据
-      // param.append('chunk','0');//添加form表单中其他数据
-      console.log('param------', param.get('file'))
-
-      home.upImg({ file: param }).then(res => {
-        console.log('res-------', res)
-      })
-      //添加请求头
-      // this.axios.post('http://upload.qiniu.com/',param,config)
-      // .then(response=>{
-      //   console.log(response.data);
-      // })
     },
     getUserInfo() {
       home.getUserInfo().then(res => {
         this.userInfo = res.data.userInfo
+        if (this.userInfo.head_img && this.userInfo.head_img != '') {
+          this.avator = this.userInfo.head_img
+        } else {
+          this.avator = require('@/assets/images/profile_avator01.png')
+        }
       })
-    },
-    upSuccess(res, file) {
-      this.userInfo.head_img = res.data.full_path
-    },
-    failUp() {}
+    }
   }
 }
 </script>
