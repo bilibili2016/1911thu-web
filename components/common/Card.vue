01556a23f4636d4ec17fb24fa839cfcbea93af0b
ZhangShuai <ZhangS@EDZdeMacBook-Pro.local>
2018-06-19 18:05:49 +0800

    update

--- components/common/Card.vue
+++ components/common/Card.vue
@@ -263,7 +263,7 @@
           <div class="particularss fr">
             <div class="currentclum">
               <h4>{{courseList.title}}</h4>
-              <div>
+              <div v-if="courseList.is_study === 0">
                 <span class="fl coursenum">
                   <span>{{courseList.curriculum_time}}课时</span><img src="@/assets/images/home_num.png" alt=""> {{courseList.study_number}}</span>
                 <span class="rate">
@@ -271,8 +271,12 @@
                 </span>
                 <span class="coins">￥ {{courseList.present_price}}</span>
               </div>
+              <div class="clum" v-else>
+                <span class="fl coursenum">
+                  <img src="@/assets/images/home_num.png" alt=""> {{courseList.study_number}}人正在学习</span>
+              </div>
             </div>
-            <div class="study clearfix">
+            <div class="study clearfix" v-if="courseList.is_study === 0">
               <p>{{courseList.introduction}}</p>
               <div class="common-button">
                 <div v-if="isAuthenticated">
@@ -281,6 +285,21 @@
                 </div>
                 <div v-else>
                   <el-button type="primary" plain @click="goBuy()" v-if="privileMsg === false">立即购买</el-button>
+                </div>
+              </div>
+            </div>
+            <div class="study clearfix bought" v-else>
+              <h4 class="clearfix">
+                <p>50分钟</p>
+                <p>已学时长</p>
+              </h4>
+              <div class="common-button">
+                <div>
+                  <el-button type="primary" plain @click="goLink(linkdata)">继续学习</el-button>
+                </div>
+                <div class="lineProgress">
+                  <h5>已学习{{courseList.percent}}%</h5>
+                  <el-progress stroke-width="14" color="#6417a6" :show-text="false" :percentage="courseList.percent"></el-progress>
                 </div>
               </div>
             </div>
@@ -1245,14 +1264,21 @@ export default {
           color: #222;
           height: 54px;
           line-height: 54px;
-          margin-bottom: 30px;
+          margin-bottom: 20px;
         }
         h4:hover {
           color: #8f4acb;
           cursor: pointer;
         }
+        .clum {
+          height: 25px;
+          .coursenum {
+            padding: 0;
+          }
+        }
         .coursenum {
           width: auto;
+          line-height: 25px;
           padding-top: 12px;
           cursor: pointer;
           span {
@@ -1268,7 +1294,7 @@ export default {
           padding-right: 20px;
         }
         .rate {
-          padding-top: 10px;
+          padding-top: 14px;
         }
         p {
           font-size: 14px;
@@ -1353,6 +1379,17 @@ export default {
           line-height: 30px;
           margin-bottom: 20px;
         }
+        h4 {
+          float: left;
+          color: #222;
+          p:first-child {
+            font-size: 20px;
+            margin: 0;
+          }
+          p:last-child {
+            font-size: 14px;
+          }
+        }
         .common-button .is-plain {
           border-radius: 20px;
           border-color: #6417a6;
@@ -1362,6 +1399,32 @@ export default {
           &:hover {
             color: #fff;
             border-color: #8f4acb;
+          }
+        }
+        &.bought {
+          .common-button .is-plain {
+            margin-top: 30px;
+          }
+          .lineProgress {
+            clear: both;
+            width: 100%;
+            padding-top: 45px;
+            h5 {
+              width: 100%;
+              line-height: 30px;
+              text-align: right;
+              font-size: 14px;
+              color: #666;
+            }
+            .el-progress-bar {
+              .el-progress-bar__outer {
+                height: 14px;
+                background-color: #ffdb5f;
+              }
+              .el-progress-bar__inner {
+                background-color: #6417a6;
+              }
+            }
           }
         }
       }
