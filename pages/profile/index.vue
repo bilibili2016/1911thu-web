9459c709883cf741d2699298fde945d9d48cd298
zhaoliangliang <zhaoliangliang@1911thu.com>
2018-06-28 09:57:42 +0800

    update

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -147,17 +147,11 @@
             </div>
           </div>
         </el-tab-pane>
-        <!-- 绑定企业Id -->
+        <!-- 绑定机构Id -->
         <el-tab-pane name="tab-seventh">
           <span slot="label" class="tabList">
-            <i class="icon-company"></i> 绑定企业ID</span>
+            <i class="icon-company"></i> 绑定机构ID</span>
           <v-companyId :cpnData="companyData"></v-companyId>
-          <div class="content">
-            <div class="noCourse" v-if="isShowNoCourse">
-              <img :src="noMsgImg" alt="">
-              <h4>抱歉，现在还没有已经绑定的课程呦~</h4>
-            </div>
-          </div>
         </el-tab-pane>
         <!-- 专属邀请码 -->
         <el-tab-pane name="tab-eighth" v-if="codeData.length">
