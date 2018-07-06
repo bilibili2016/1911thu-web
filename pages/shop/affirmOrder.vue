1664304a1bf44481e4f1a2c28a720250f76f8eac
ZhangShuai <ZhangS@EDZdeMacBook-Pro.local>
2018-07-07 00:28:06 +0800

    update

--- pages/shop/affirmOrder.vue
+++ pages/shop/affirmOrder.vue
@@ -884,8 +884,8 @@ export default {
               this.invoiceForm.companyname = this.zzTicketForm.companyname
               this.invoiceForm.number = this.zzTicketForm.number
               this.invoiceForm.address = this.zzTicketForm.address
-              this.invoiceForm.radio = Number(this.zzTicketForm.radio)
               this.invoiceForm.others = this.zzTicketForm.others
+              this.invoiceForm.radio = Number(this.zzTicketForm.radio)
               this.invoiceForm.ticket = false
               this.isShowTicket = true
               this.commitOrders.ticketId = res.data.invoice_id
@@ -948,7 +948,6 @@ export default {
               this.ticketForm.area = res.data.area
               this.zzTicketForm.address = res.data.address
               this.zzTicketForm.others = res.data.content
-              this.TicketForm.others = res.data.content
               this.zzTicketForm.radio = Number(res.data.content_type)
               this.commitOrders.ticketId = res.data.id
               if (this.zzTicketForm.radio == 2) {
