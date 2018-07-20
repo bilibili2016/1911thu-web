443afe129d20ee93e537ce80060db8b0d137c35a
zhaoliangliang <zhaoliangliang@1911thu.com>
2018-07-20 17:30:35 +0800

    测试ie

--- plugins/main.js
+++ plugins/main.js
@@ -27,3 +27,38 @@ eventBus.install = function(Vue) {
 }
 
 Vue.use(eventBus)
+
+// var userAgent = navigator.userAgent // 取得浏览器的 userAgent 字符串
+// if (
+//   userAgent.indexOf('compatible') > -1 &&
+//   userAgent.indexOf('MSIE') > -1 &&
+//   !userAgent.indexOf('Opera') > -1
+// ) {
+//   alert('IE')
+//   // do IE something
+// } else {
+//   alert('非IE')
+// }
+
+var isIE = function(ver) {
+  var b = document.createElement('b')
+  b.innerHTML = '<!--[if IE ' + ver + ']><i></i><![endif]-->'
+  return b.getElementsByTagName('i').length === 1
+}
+if (isIE(6)) {
+  // IE 6
+  console.log('6')
+}
+if (isIE(7)) {
+  // IE 6
+  console.log('7')
+}
+if (isIE(8)) {
+  // IE 6
+  console.log('8')
+}
+// ...
+if (isIE(9)) {
+  // IE 9
+  console.log('9')
+}
