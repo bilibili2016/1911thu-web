5dcf92c9d4412405501bfeb637a6bffdb3c64314
zhaoliangliang <zhaoliangliang@1911thu.com>
2018-07-20 18:25:24 +0800

    测试ie

---
+++ pages/other/ie.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="img-box">
+      <img src="@/assets/images/ie.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="scss">
+.img-box {
+  width: 500px;
+  height: 400px;
+  margin: 100px auto;
+  img {
+    width: 500px;
+    height: 400px;
+  }
+  min-height: 500px;
+}
+</style>
