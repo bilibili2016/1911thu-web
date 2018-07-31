f209904f392d1864aa83e0403306a09fc226ee6c
zhaoliangliang <zhaoliangliang@1911thu.com>
2018-07-31 13:11:33 +0800

    update

--- pages/project/partypolicy.vue
+++ pages/project/partypolicy.vue
@@ -1,163 +1,363 @@
 <template>
-  <div class="project">
-    <div class="distinctive">
-      <div class="lietOne">
-        <div class="dttImg fl">
-          <img src="~assets/images/distinctive_1.png" alt="">
-        </div>
-        <h4 class="fl">项目特色</h4>
-        <div class="inner">
-          <ul>
-            <li>● 权威性：整合国内一流大学专家、各领域行业精英零距离交流；</li>
-            <li>● 实战性：经典政府案例研究，同学分享讨论，专家精辟点评；</li>
-            <li>● 创新性：培养管理思维与创新能力，提升核心竞争力；</li>
-            <li>● 实用性：针对性探讨研习，推动党政管理从成功走向卓越；</li>
-            <li>● 共享性：高峰论坛名家对话，同窗智囊经验分享。</li>
-          </ul>
+  <div>
+    <div class="policyPrograme">
+      <div class="banner">
+        <div class="text">
+          <p class="small-title">
+            <span class="line"></span>
+            <span class="desc">线上线下混合模式</span>
+            <span class="line"></span>
+          </p>
+          <p class="title">党政干部综合能力提升</p>
         </div>
       </div>
-      <div class="lietOne">
-        <div class="dttImg fl">
-          <img src="~assets/images/distinctive_2.png" alt="">
+      <div class="content-one">
+        <div class="program-bg">
+          <div class="title">
+            <img class="titlt-icon-left" src="@/assets/images/policy-pic01.png" alt="">
+            <span class="text">项目背景</span>
+            <img class="titlt-icon-right" src="@/assets/images/policy-pic02.png" alt="">
+          </div>
+          <div class="desc">
+            <p>干部教育培训是建设高素质干部队伍的先导性、基础性、战略性工程，在推进中国特色社会主义伟大事业和党的建设中具有不可替代的地位和作用。中央组织部办公厅印发的《干部教育培训工作条例》中明确强调，干部教育培训是传播党的理论和路线方针政策的重要载体，是建设学习型、服务型、创新型马克思主义执政党的有效抓手，是提高干部教育培训质量效益的基础保障。</p>
+            <p>1911学堂是1911集团联合清华大学及国内外著名高校共同发起的在线继续教育平台，是面向未来的战略部署。1911学堂旨在通过整合清华大学及国内外著名高校优质的教育资源，将继续教育与互联网完美结合，形成系统性、针对性、实用性的继续教育培养服务体系，以线上线下相结合的模式，为党政干部与企事业单位高管提供终身教育解决方案，最终建设一所面向未来的终身学堂。 </p>
+            <p> “党政干部综合能力提升线上线下混合式（SPOC）高级研修班（简称党政干部综合能力提升混合式高级研修班）”设置了四大课程体系,包括:宏观政策与深化改革，公共管理与创新发展，人文素养与通用能力提升，新时代党政建设与科学管理。四大课程体系各具特色又相互促进，助力党政干部加快知识更新、转变思维方式、提高学习能力、改善行为模式，为政治经济和社会的协调发展提供优质高效的服务。</p>
+
+          </div>
         </div>
-        <h4 class="fl">学习收获</h4>
-        <div class="inner">
-          <ul>
-            <li> ● 树立把握全局的战略思维；</li>
-            <li> ● 增加勇于开拓的创新精神；</li>
-            <li> ● 夯实党政管理的专业知识；</li>
-            <li> ● 提高果断科学的决策能力；</li>
-            <li> ● 增强统筹协调的组织才能；</li>
-            <li> ● 提升党政管理的人文素养。</li>
-          </ul>
+        <div class="study-info">
+          <div class="title">
+            <img class="titlt-icon-left" src="@/assets/images/policy-pic01.png" alt="">
+            <span class="text">学习对象</span>
+            <img class="titlt-icon-right" src="@/assets/images/policy-pic02.png" alt="">
+          </div>
+          <div class="info clearfix">
+            <div class="item item-one">
+              <img src="http://papn9j3ys.bkt.clouddn.com/policy-icon1.png" alt="">
+              <p>各级党政机关领导干部</p>
+            </div>
+            <div class="item item-two">
+              <img src="http://papn9j3ys.bkt.clouddn.com/policy-icon2.png" alt="">
+              <p>事业单位领导干部</p>
+            </div>
+            <div class="item item-three">
+              <img src="http://papn9j3ys.bkt.clouddn.com/policy-icon3.png" alt="">
+              <p>中青年后备干部</p>
+            </div>
+            <div class="item item-four">
+              <img src="http://papn9j3ys.bkt.clouddn.com/policy-icon4.png" alt="">
+              <p>基层干部等</p>
+            </div>
+          </div>
         </div>
       </div>
+      <div class="content-two">
+        <div class="top-con">
+          <div class="title">
+            <img class="titlt-icon-left" src="@/assets/images/policy-pic01.png" alt="">
+            <span class="text">混合式教学方式</span>
+            <img class="titlt-icon-right" src="@/assets/images/policy-pic02.png" alt="">
+          </div>
+          <p class="desc">1911学堂在新时代背景中不断创新，除了传统的面授学习和网络学习以外，结合党政干部的职业特点和学习需求，研发推出了 线上+线下的混合式教学培训体系，即融合了课堂面授与在线学习的SPOC模式。
+          </p>
+        </div>
+        <div class="teach-info">
+          <div class="item clearfix">
+            <div class="left">
+              <div class="down">
+                <div class="up"></div>
+              </div>
+
+            </div>
+            <div class="right">
+              <p class="title-con">
+                线上+线下的混合式教学方式
+              </p>
+              <p class="text">
+                线上网络学习：党政干部学员登录1911学堂（www.1911edu.com）教学平台在线学习相关课程内容。 <br />线下课堂面授：通过课堂面对面的讨论交流、互动答疑以及现场教学、参观考察等方式，针对课程中存疑的内容结合工作实践进行全面分析，并解决实际的问题。 <br />1911学堂研发的线上+线下的混合式教学方式，真正实现了党政干部自主学习、探索式学习和交互式学习的有机结合，实现了从传统灌输式学习向自我分析问题、解决问题的转变，促进了干部教育培训的创新发展。
+
+              </p>
+            </div>
+          </div>
+          <div class="item clearfix">
+            <div class="left">
+              <div class="down">
+                <div class="up"></div>
+              </div>
+
+            </div>
+            <div class="right">
+              <p class="title-con">
+                学时
+              </p>
+              <p class="text">
+                共62学时，每学时=45分钟（下同）。<br /> 线上网络学习：计50学时；线下课堂面授：计12学时
+              </p>
+            </div>
+          </div>
+          <div class="item clearfix">
+            <div class="left">
+              <div class="down">
+                <div class="up"></div>
+              </div>
+
+            </div>
+            <div class="right">
+              <p class="title-con">
+                学费
+              </p>
+              <p class="text">
+                按每班50人计，一个班（不超50人）综合费用为：￥19万/班。<br /> 线下培训费用包含资料费、授课费、场地费，不含往返交通费、食宿费等。
+                <br /> 1911学堂可提供增值税专用发票。
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <di class="content-three">
+        <div class="title">
+          <img class="titlt-icon-left" src="@/assets/images/policy-pic01.png" alt="">
+          <span class="text">学习方式及优势</span>
+          <img class="titlt-icon-right" src="@/assets/images/policy-pic02.png" alt="">
+        </div>
+        <div class="study-info clearfix">
+          <div class="info info1">
+            <div class="item clearfix">
+              <div class="left">
+                <div class="down">
+                  <div class="up"></div>
+                </div>
+
+              </div>
+              <div class="right">
+                <p class="title-con">学习方式包括：</p>
+                <p class="text">
+                  在线学习、小组研讨、行动学习、翻转课堂、深度学习、跨界学习、个性化学习、沉浸式学习、探索式学习、交互式学习、自主学习、同伴学习； <br />课堂面授、专题讲座、现场教学、混合教学、参观考察、标杆学习、专家指导、互动交流、成果汇报等。
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="info info2">
+            <div class="item clearfix">
+              <div class="left">
+                <div class="down">
+                  <div class="up"></div>
+                </div>
+
+              </div>
+              <div class="right">
+                <p class="title-con">学习方式的优势：</p>
+                <p class="text">
+                  集中面授培训与网络在线培训相结合；<br /> 单位时间内比传统的面授培训学到更多更权威的内容；
+                  <br /> 以内容驱动为中心、以解决问题为导向；
+                  <br /> 线上学习与线下研讨相结合；
+                  <br /> 理论学习与实践反思相结合；
+                  <br /> 自主学习与同伴学习相结合；
+                  <br /> 基于大数据分析的个性化学习；
+                </p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        <div class="course">
+          <div class="title">
+            <img class="titlt-icon-left" src="@/assets/images/policy-pic01.png" alt="">
+            <span class="text">授课师资</span>
+            <img class="titlt-icon-right" src="@/assets/images/policy-pic02.png" alt="">
+          </div>
+          <p class="course-info">师资是一个培训项目质量的关键，良好的师资队伍是学员学习收益最大化的保障。本项目授课师资由三支队伍组成：</p>
+          <div class="course-item clearfix">
+            <div class="item clearfix">
+              <div class="num">
+                1
+              </div>
+              <div class="desc">
+                来自清华北大等一流高等学府在公共管理领域有深厚理论功底的资深教授；
+
+              </div>
+            </div>
+            <div class="item clearfix">
+              <div class="num">
+                2
+              </div>
+              <div class="desc">
+                来自各领域的行业精英、具有公共管理与实践经验的咨询顾问和实战型专家；
+              </div>
+            </div>
+            <div class="item clearfix">
+              <div class="num">
+                3
+              </div>
+              <div class="desc">
+                制定政策、解读政策走向的专家及把握政策趋势的智囊团队。
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </di>
     </div>
-    <div class="system">
-      <!-- 课程体系 -->
-      <div class="courseSystem">
-        <h3>课程体系</h3>
-        <p>该项目共四大课程体系，九个课程模块，总计62学时，包含： </p>
-        <p>线上网络课程：八个课程模块，计50学时。线下面授课程：一个课程模块，计12学时。</p>
-        <div class="mode">
-          <div class="modeList">
-            <div class="modeTop">
-              <span class="fl">1</span>
-              <span class="fr">体系一：宏观政策与深 化改革</span>
-            </div>
-            <div class="modeBottom">
-              <span class="fl">3</span>
-              <span class="fr">体系三：人文素养与通 用能力提升</span>
-            </div>
+    <div class="project">
+      <div class="distinctive">
+        <div class="lietOne">
+          <div class="dttImg fl">
+            <img src="~assets/images/distinctive_1.png" alt="">
           </div>
-          <div class="modeCenter">
-            <p>党政干部综合</p>
-            <p>能力提升混合式</p>
-            <p>高级研修班</p>
+          <h4 class="fl">项目特色</h4>
+          <div class="inner">
+            <ul>
+              <li>● 权威性：整合国内一流大学专家、各领域行业精英零距离交流；</li>
+              <li>● 实战性：经典政府案例研究，同学分享讨论，专家精辟点评；</li>
+              <li>● 创新性：培养管理思维与创新能力，提升核心竞争力；</li>
+              <li>● 实用性：针对性探讨研习，推动党政管理从成功走向卓越；</li>
+              <li>● 共享性：高峰论坛名家对话，同窗智囊经验分享。</li>
+            </ul>
           </div>
-          <div class="modeList">
-            <div class="modeTop">
-              <span class="fl">1</span>
-              <span class="fr">体系一：宏观政策与深 化改革</span>
-            </div>
-            <div class="modeBottom">
-              <span class="fl">3</span>
-              <span class="fr">体系三：人文素养与通 用能力提升</span>
-            </div>
+        </div>
+        <div class="lietOne">
+          <div class="dttImg fl">
+            <img src="~assets/images/distinctive_2.png" alt="">
+          </div>
+          <h4 class="fl">学习收获</h4>
+          <div class="inner">
+            <ul>
+              <li> ● 树立把握全局的战略思维；</li>
+              <li> ● 增加勇于开拓的创新精神；</li>
+              <li> ● 夯实党政管理的专业知识；</li>
+              <li> ● 提高果断科学的决策能力；</li>
+              <li> ● 增强统筹协调的组织才能；</li>
+              <li> ● 提升党政管理的人文素养。</li>
+            </ul>
           </div>
         </div>
       </div>
-      <!-- 线上学习课程 -->
-      <div class="studyCourse">
-        <h3>线上学习课程</h3>
-        <div class="studyInfo">
-          <h5>
-            <span class="studyTitle">三个体系</span>
-            <span>宏观政策与深化改革，公共管理与创新发展，人文素养与通用能力提升。</span>
-          </h5>
-          <h5>
-            <span class="studyTitle">学习方式</span>
-            <span>在线自主学习+同伴学习+补充作业+课后测试。</span>
-          </h5>
-          <h5>
-            <span class="studyTitle">学习时间</span>
-            <span>50学时，32门课；</span>
-          </h5>
-          <p>自注册账号之日起，登录1911学堂网站（www.1911edu.com）或APP教学平台在线学习相关课程内容，有效期12个月。</p>
+      <div class="system">
+        <!-- 课程体系 -->
+        <div class="courseSystem">
+          <h3>课程体系</h3>
+          <p>该项目共四大课程体系，九个课程模块，总计62学时，包含： </p>
+          <p>线上网络课程：八个课程模块，计50学时。线下面授课程：一个课程模块，计12学时。</p>
+          <div class="mode">
+            <div class="modeList">
+              <div class="modeTop">
+                <span class="fl">1</span>
+                <span class="fr">体系一：宏观政策与深 化改革</span>
+              </div>
+              <div class="modeBottom">
+                <span class="fl">3</span>
+                <span class="fr">体系三：人文素养与通 用能力提升</span>
+              </div>
+            </div>
+            <div class="modeCenter">
+              <p>党政干部综合</p>
+              <p>能力提升混合式</p>
+              <p>高级研修班</p>
+            </div>
+            <div class="modeList">
+              <div class="modeTop">
+                <span class="fl">1</span>
+                <span class="fr">体系一：宏观政策与深 化改革</span>
+              </div>
+              <div class="modeBottom">
+                <span class="fl">3</span>
+                <span class="fr">体系三：人文素养与通 用能力提升</span>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      <!-- 体系 表格1 -->
-      <div class="table">
-        <table cellpadding="0" cellspacing="0" width="100%">
-          <tr class="titleOne">
-            <th colspan="4">体系一：宏观政策与深化改革（线上课程）</th>
-          </tr>
-          <tr class="titleTwo">
-            <th colspan="2" class="checked">模块一：宏观政策解读</th>
-            <th colspan="2">模块一：新时代深化改革</th>
-          </tr>
-          <tr class="titleThree">
-            <th>课程名称</th>
-            <th>主讲人</th>
-            <th>职务</th>
-            <th>学时</th>
-          </tr>
-          <tr>
-            <td>当前经济形势判断与区域经济发展</td>
-            <td>马晓河</td>
-            <td>国家发改委宏观院原副院长、研究员</td>
-            <td>1</td>
-          </tr>
-        </table>
-      </div>
-      <!-- 体系 表格2 -->
-      <div class="table tableTwo">
-        <table cellpadding="0" cellspacing="0" width="100%">
-          <tr class="titleOne">
-            <th colspan="4">体系一：宏观政策与深化改革（线上课程）</th>
-          </tr>
-          <tr class="titleTwo">
-            <th colspan="2" class="checked">模块一：宏观政策解读</th>
-            <th colspan="2">模块一：新时代深化改革</th>
-          </tr>
-          <tr class="titleThree">
-            <th>课程名称</th>
-            <th>主讲人</th>
-            <th>职务</th>
-            <th>学时</th>
-          </tr>
-          <tr>
-            <td>当前经济形势判断与区域经济发展</td>
-            <td>马晓河</td>
-            <td>国家发改委宏观院原副院长、研究员</td>
-            <td>1</td>
-          </tr>
-        </table>
-      </div>
-      <!-- 体系 表格3 -->
-      <div class="table tableThree">
-        <table cellpadding="0" cellspacing="0" width="100%">
-          <tr class="titleOne">
-            <th colspan="4">体系二：公共管理与创新发展（线上课程）</th>
-          </tr>
-          <tr class="titleTwo">
-            <th colspan="2" class="checked">模块一：领导人才管理</th>
-            <th colspan="2">模块二：城市规划与乡村振兴</th>
-            <th colspan="2">模块三：地方财政解析</th>
-          </tr>
-          <tr class="titleThree">
-            <th>课程名称</th>
-            <th>主讲人</th>
-            <th>职务</th>
-            <th>学时</th>
-          </tr>
-          <tr>
-            <td>当前经济形势判断与区域经济发展</td>
-            <td>马晓河</td>
-            <td>国家发改委宏观院原副院长、研究员</td>
-            <td>1</td>
-          </tr>
-        </table>
+        <!-- 线上学习课程 -->
+        <div class="studyCourse">
+          <h3>线上学习课程</h3>
+          <div class="studyInfo">
+            <h5>
+              <span class="studyTitle">三个体系</span>
+              <span>宏观政策与深化改革，公共管理与创新发展，人文素养与通用能力提升。</span>
+            </h5>
+            <h5>
+              <span class="studyTitle">学习方式</span>
+              <span>在线自主学习+同伴学习+补充作业+课后测试。</span>
+            </h5>
+            <h5>
+              <span class="studyTitle">学习时间</span>
+              <span>50学时，32门课；</span>
+            </h5>
+            <p>自注册账号之日起，登录1911学堂网站（www.1911edu.com）或APP教学平台在线学习相关课程内容，有效期12个月。</p>
+          </div>
+        </div>
+        <!-- 体系 表格1 -->
+        <div class="table">
+          <table cellpadding="0" cellspacing="0" width="100%">
+            <tr class="titleOne">
+              <th colspan="4">体系一：宏观政策与深化改革（线上课程）</th>
+            </tr>
+            <tr class="titleTwo">
+              <th colspan="2" class="checked">模块一：宏观政策解读</th>
+              <th colspan="2">模块一：新时代深化改革</th>
+            </tr>
+            <tr class="titleThree">
+              <th>课程名称</th>
+              <th>主讲人</th>
+              <th>职务</th>
+              <th>学时</th>
+            </tr>
+            <tr>
+              <td>当前经济形势判断与区域经济发展</td>
+              <td>马晓河</td>
+              <td>国家发改委宏观院原副院长、研究员</td>
+              <td>1</td>
+            </tr>
+          </table>
+        </div>
+        <!-- 体系 表格2 -->
+        <div class="table tableTwo">
+          <table cellpadding="0" cellspacing="0" width="100%">
+            <tr class="titleOne">
+              <th colspan="4">体系一：宏观政策与深化改革（线上课程）</th>
+            </tr>
+            <tr class="titleTwo">
+              <th colspan="2" class="checked">模块一：宏观政策解读</th>
+              <th colspan="2">模块一：新时代深化改革</th>
+            </tr>
+            <tr class="titleThree">
+              <th>课程名称</th>
+              <th>主讲人</th>
+              <th>职务</th>
+              <th>学时</th>
+            </tr>
+            <tr>
+              <td>当前经济形势判断与区域经济发展</td>
+              <td>马晓河</td>
+              <td>国家发改委宏观院原副院长、研究员</td>
+              <td>1</td>
+            </tr>
+          </table>
+        </div>
+        <!-- 体系 表格3 -->
+        <div class="table tableThree">
+          <table cellpadding="0" cellspacing="0" width="100%">
+            <tr class="titleOne">
+              <th colspan="4">体系二：公共管理与创新发展（线上课程）</th>
+            </tr>
+            <tr class="titleTwo">
+              <th colspan="2" class="checked">模块一：领导人才管理</th>
+              <th colspan="2">模块二：城市规划与乡村振兴</th>
+              <th colspan="2">模块三：地方财政解析</th>
+            </tr>
+            <tr class="titleThree">
+              <th>课程名称</th>
+              <th>主讲人</th>
+              <th>职务</th>
+              <th>学时</th>
+            </tr>
+            <tr>
+              <td>当前经济形势判断与区域经济发展</td>
+              <td>马晓河</td>
+              <td>国家发改委宏观院原副院长、研究员</td>
+              <td>1</td>
+            </tr>
+          </table>
+        </div>
       </div>
     </div>
   </div>
