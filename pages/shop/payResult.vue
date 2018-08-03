be7c5b356922c08ff6ec3dcf071ef0c4b9cba410
zhaoliangliang <zhaoliangliang@1911thu.com>
2018-08-03 15:33:15 +0800

    增加购物有车底部判断

--- pages/shop/payResult.vue
+++ pages/shop/payResult.vue
@@ -12,18 +12,6 @@
           <span>支付金额：￥{{payCompleteData.order_amount}}</span>
         </p>
       </div>
-      <!-- <div class="restltMsg restltWord" v-else>
-        <h6>订单：1806201053081435</h6>
-        <p>支付金额：￥2250.00</p>
-        <p>错误原因：账户余额不足</p>
-      </div> -->
-      <!-- <h5 v-if="success">
-        <span @click="choiceCourse">继续选课</span>
-        <span>查看订单</span>
-      </h5>
-      <h5 v-else>
-        <span>重新下单</span>
-      </h5> -->
       <h5>
         <span @click="choiceCourse">继续选课</span>
         <span @click="goMycourse('tab-third')">查看订单</span>
