16419794c439b7952bc8f32ff7325733fa05cf67
zhaoliangliang <zhaoliangliang@1911thu.com>
2018-08-03 18:36:50 +0800

    update

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1025,7 +1025,7 @@ export default {
         height: 55px;
         line-height: 55px;
         background-color: #fff;
-        color: #222;
+        // color: #222;
         border-bottom: 1px solid #e8d6f7;
         span {
           vertical-align: middle;
@@ -1059,7 +1059,7 @@ export default {
               height: 50px;
               line-height: 50px;
               font-size: 14px;
-              color: #222;
+              // color: #222;
               .info-fr,
               .info-fl {
                 width: 50%;
@@ -1157,7 +1157,7 @@ export default {
             p {
               font-size: 14px;
               text-align: right;
-              color: #222;
+              // color: #222;
               line-height: 30px;
             }
             h4 {
