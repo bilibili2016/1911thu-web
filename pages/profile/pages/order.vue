f34e573ddb0007551019f1c0c9e4fc91a0f5e8ee
liyuru <liyuru@1911Thu.com>
2018-08-06 13:12:32 +0800

    整合sdk

--- pages/profile/pages/order.vue
+++ pages/profile/pages/order.vue
@@ -58,7 +58,7 @@
 </template>
 
 <script>
-import { home } from '~/lib/v1_sdk/index'
+import { order } from '~/lib/v1_sdk/index'
 import { mapActions } from 'vuex'
 import { store as persistStore } from '~/lib/core/store'
 export default {
@@ -94,7 +94,7 @@ export default {
     goShopping(id) {
       this.orderForm.ids = id
       return new Promise((resolve, reject) => {
-        home.buyAgain(this.orderForm).then(response => {
+        order.buyAgain(this.orderForm).then(response => {
           if (response.status === 0) {
             this.$router.push('/shop/shoppingCart')
           } else {
@@ -116,7 +116,7 @@ export default {
       //取消订单
       this.orderForm.ids = id
       return new Promise((resolve, reject) => {
-        home.cancelOrder(this.orderForm).then(response => {
+        order.cancelOrder(this.orderForm).then(response => {
           if (response.status === 0) {
             this.$emit('handleUpdate', true)
             this.$message({
