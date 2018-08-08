c47ff78f8ff8d89e64162cbc5f2c9e1be1258f96
liyuru <liyuru@1911Thu.com>
2018-08-08 11:42:04 +0800

    小组件抽离

---
+++ pages/course/components/Evaluate.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="commentator clearfix" v-for="(item,index) in evaluteData" :key="index" v-if="index<3">
+            <img class="fl" :src="item.head_img" alt="">
+            <div class="fl">
+                <p style="margin:3px 0 8px;">{{item.nick_name}}</p>
+                <p class="time">{{item.create_time}}</p>
+            </div>
+            <div style="margin-top:10px;">
+                <el-rate disabled v-model="item.score" class="itemBox-rate fr"></el-rate>
+            </div>
+            <h5 v-if="item.tags ===''">{{item.evaluate_content}}</h5>
+            <h5 v-else-if="item.evaluate_content===''">{{item.tags}}</h5>
+            <h5 v-else>{{item.tags}}，{{item.evaluate_content}}</h5>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['evaluteData']
+}
+</script>
+
+<style scoped>
+</style>
