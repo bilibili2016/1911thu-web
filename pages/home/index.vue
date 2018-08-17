01399c1f3feb99326124de52b312596f43ab6370
ZhangShuai <ZhangS@EDZdeMacBook-Pro.local>
2018-08-17 11:43:10 +0800

    update

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -277,6 +277,8 @@ export default {
     this.getNewCourseList()
   },
   mounted() {
+    document.getElementsByClassName('headerBox')[0].style.display = 'inline'
+    document.getElementsByClassName('footerBox')[0].style.display = 'inline'
     this.windowWidth = document.documentElement.clientWidth
 
     this.$bus.$on('loginMsg', data => {
