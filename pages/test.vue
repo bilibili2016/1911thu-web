a2301502173153faf72194ae19b0ca113cc8e37e
zhaoliangliang <zhaoliangliang@1911thu.com>
2018-08-29 18:28:27 +0800

    同步自定义项目

---
+++ pages/test.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    aa(data) {
+      const step = step1 => step2 => param => step2(step1(param))
+      step(data => (this.getCodeListForm.code = data))(this.bb)(data)
+    },
+    bb(item) {
+      console.log(this.getCodeListForm.code, '我草函数行编程2')
+      // this.getCodeListForm.code = this.getCodeListForm.code
+      profileHome.getRecordList(this.getCodeListForm).then(response => {
+        // this.recordData = response.data.usedInvitationCodeList
+        // this.getCodeListForm.code = ''
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
