d16b5f63583d77b997dba753e8f83732f4da6ae4
ZhangShuai <ZhangS@zhangshuai.local>
2018-09-03 16:42:28 +0800

    课程详情tab

--- pages/course/coursedetail/CourseCatalog.vue
+++ pages/course/coursedetail/CourseCatalog.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    <el-tabs v-model="activeName">
-      <el-tab-pane label="介绍" name="first">
+    <el-tabs v-model="activeNames">
+      <el-tab-pane label="课程简介" name="first">
         <div class="detail descript" v-html="courseList.content" v-loading="loadMsg"></div>
       </el-tab-pane>
-      <el-tab-pane label="目录" name="second">
+      <el-tab-pane label="课程目录" name="second">
         <v-line :catalogs="catalogs" :privileMsg="privileMsg" :config="config" :changeImg="changeImg"></v-line>
+      </el-tab-pane>
+      <el-tab-pane label="用户评论" name="third">
+        <v-userevaluate :totalEvaluateInfo="totalEvaluateInfo" :commentator="commentator" :loadEvaluate="loadEvaluate" :pageCount="pageCount" :sumUserStart="sumUserStart" :pagemsg="pagemsg" @pagechange="pagechange"></v-userevaluate>
       </el-tab-pane>
     </el-tabs>
   </div>
@@ -16,7 +19,12 @@
  * 组件名字
  */
 import CustomLine from '@/components/common/Line.vue'
+import UserEvaluate from '@/pages/course/coursedetail/UserEvaluate.vue'
 export default {
+  components: {
+    'v-line': CustomLine,
+    'v-userevaluate': UserEvaluate
+  },
   props: [
     'activeName',
     'courseList',
@@ -24,10 +32,27 @@ export default {
     'catalogs',
     'privileMsg',
     'config',
-    'changeImg'
+    'changeImg',
+    'totalEvaluateInfo',
+    'commentator',
+    'loadEvaluate',
+    'pageCount',
+    'sumUserStart',
+    'pagemsg'
   ],
-  components: {
-    'v-line': CustomLine
+  data() {
+    return {
+      activeNames: 'first'
+    }
+  },
+  methods: {
+    // 分页事件
+    pagechange(val) {
+      this.$emit('pagechange', val)
+    }
+  },
+  mounted() {
+    this.activeNames = this.activeName
   }
 }
 </script>
