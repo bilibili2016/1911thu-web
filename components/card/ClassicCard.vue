917f4bec385f888fcb61d50f22323019d1d5e015
liyuru <liyuru@1911Thu.com>
2018-10-23 17:52:26 +0800

    update

--- components/card/ClassicCard.vue
+++ components/card/ClassicCard.vue
@@ -4,22 +4,26 @@
       <div class="fl">
         <img class="teacherImg" :src="course.teacher_picture" alt="">
       </div>
-        <div class="fr courseInfo">
-          <h4 :title="course.title">{{course.title}}</h4>
-          <!-- <h5>{{course.deputy_title}}</h5> -->
-          <h5>{{course.teacher_name+'&nbsp;&nbsp;'+course.graduate}}</h5>
+      <div class="fr courseInfo">
+        <h4 :title="course.title">{{course.title}}</h4>
+        <!-- <h5>{{course.deputy_title}}</h5> -->
+        <h5>{{course.teacher_name+'&nbsp;&nbsp;'+course.graduate}}</h5>
 
-          <p v-if="course.is_free === '1'" class="money">￥{{course.present_price}}</p>
-          <p v-else class="money">免费</p>
-          <h6 class="clearfix">
-            <p class="fl">{{course.study_time}}学时 <img :src="stydyNum" alt=""> {{course.study_number}}</p>
-              <p class="fr">
-                <el-rate disabled v-model="course.score" class="itemBox-rate"></el-rate>
-              </p>
-          </h6>
-        </div>
+        <p v-if="course.is_free === '1'" class="money">￥{{course.present_price}}</p>
+        <p v-if="course.is_free === '2'" class="money">
+          <span>限免</span>
+          <span style="float:right">剩余{{course.free_end_time}}</span>
+        </p>
+
+        <h6 class="clearfix">
+          <p class="fl">{{course.study_time}}学时 <img :src="stydyNum" alt=""> {{course.study_number}}</p>
+          <p class="fr">
+            <el-rate disabled v-model="course.score" class="itemBox-rate"></el-rate>
+          </p>
+        </h6>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
--- pages/profile/components/myticket/detailPopup.vue
+++ pages/profile/components/myticket/detailPopup.vue
@@ -13,10 +13,14 @@
                     <div class="item-list clearfix" v-for="(item,index) in data" :key="index">
                         <div class="code">{{item.order_sn}}</div>
                         <div class="time">{{exchangeTime(item.create_time)}}</div>
-                        <div class="num">{{item.project_number}}个项目，{{item.curriculum_number}}个课程</div>
+                        <div class="num">
+                            <span v-if="item.project_number>0">{{item.project_number}}个项目</span>
+                            <span v-if="item.project_number>0 &&item.curriculum_number>0">，</span>
+                            <span v-if="item.curriculum_number>0">{{item.curriculum_number}}个课程</span>
+                            <span v-if="item.title">{{item.title}}</span>
+                        </div>
                         <div class="price">{{item.pay_amount}}元</div>
                     </div>
-
                 </div>
             </div>
         </div>
