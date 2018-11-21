09a4c0ac019bd9f2352feb4a22a99f1dbea65c14
liyuru <liyuru@1911Thu.com>
2018-11-21 14:32:30 +0800

    设置页签titletitle

--- pages/home/citySchool/schoolIntro.vue
+++ pages/home/citySchool/schoolIntro.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="schoolIntro">
-        <div class="btn-con">
-            <span @click="handleLink('/home/citySchool/schoolApplication')">立即申请</span>
-        </div>
+  <div class="schoolIntro">
+    <div class="btn-con">
+      <span @click="handleLink('/home/citySchool/schoolApplication')">立即申请</span>
     </div>
+  </div>
 </template>
 <script>
+import { matchSplits, setTitle, message } from '@/lib/util/helper'
+
 export default {
   data() {
     return {}
@@ -14,6 +16,9 @@ export default {
     handleLink(link) {
       this.$router.push(link)
     }
+  },
+  mounted() {
+    setTitle('城市分校-1911学堂')
   }
 }
 </script>
--- pages/home/citySchool/submitSuccess.vue
+++ pages/home/citySchool/submitSuccess.vue
@@ -7,6 +7,14 @@
     <div class="desc">感谢您对1911学堂城市分校的关注，我们会在48小时内与您取得联系！</div>
   </div>
 </template>
+<script>
+import { matchSplits, setTitle, message } from '@/lib/util/helper'
+export default {
+  mounted() {
+    setTitle('提交成功-1911学堂')
+  }
+}
+</script>
 <style scoped lang="scss">
 .submitSuccess {
   margin-top: 200px;
