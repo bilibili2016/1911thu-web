e1c9892c1c8591b64b0daf4224cecbaf1b802f9d
ZhangShuai <zhangshuai@1911thu.com>
2018-11-21 14:12:18 +0800

    城市分校

--- pages/home/citySchool/schoolIntro.vue
+++ pages/home/citySchool/schoolIntro.vue
@@ -1,9 +1,22 @@
 <template>
-    <div class="schoolIntro">
-        <div class="btn-con">
-            <span @click="handleLink('/home/citySchool/schoolApplication')">立即申请</span>
-        </div>
+  <div class="schoolIntro">
+    <div class="schoolBanner">
+      <h4>1911学堂城市分校校长招募计划</h4>
+      <p>城市分校校长招募计划是1911学堂推动全球布局、实现多方共赢的重大战略举措，旨在打造精英团队、加大办学力度，惠及全世界所有致力于更好职业发展的职场人士。</p>
+      <p>通过在各地1911学堂的学习，学员们能够夯实专业基础、提高决策能力、增强组织才能、打造战略思维、树立创新精神、提升人文素养、开拓人脉资源，并获得清华大学在线学习认证证书。</p>
+      <p>与此同时，在1911学堂总部的支持下，各城市分校也将在办学中获得成长与收益。</p>
     </div>
+    <div class="schoolInfo">
+      <img class="schoolImg topImg" src="http://h5-image.1911edu.com/schoolIntro2.png" alt="">
+      <div class="color">
+        <img class="schoolImg" src="http://h5-image.1911edu.com/schoolIntro3.png" alt="">
+      </div>
+      <img class="schoolImg" src="http://h5-image.1911edu.com/schoolIntro4.png" alt="">
+      <p><span>查看分校校址</span></p>
+      <img class="schoolImg" src="http://h5-image.1911edu.com/schoolIntro5.png" alt="">
+      <p><span @click="handleLink('/home/citySchool/schoolApplication')">立即申请</span></p>
+    </div>
+  </div>
 </template>
 <script>
 export default {
