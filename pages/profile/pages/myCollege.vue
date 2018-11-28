6db71071717f7d5b4001e4f098c57fd0d27dae4f
ZhangShuai <zhangshuai@1911thu.com>
2018-11-28 15:43:48 +0800

    update

--- pages/profile/pages/myCollege.vue
+++ pages/profile/pages/myCollege.vue
@@ -6,16 +6,29 @@
         <div class="top-con clearfix">
           <span>我的学院</span>
         </div>
-        <div class="collegeList" v-if="collegeListData.length > 0" v-loading="collegeLoading" :class="{ minheight : collegeLoading}">
-          <v-list :collegeListData="collegeListData"></v-list>
+        <div class="collegeList" :class="{ minheight : collegeLoading}" v-loading="collegeLoading">
+          <v-list :collegeListData="collegeListData" v-if="collegeListData.length > 0"></v-list>
         </div>
         <div class="pagination" v-if="collegePagemsg.total>11&&collegeListData.length > 0">
-          <el-pagination background layout="prev, pager, next" :page-size="collegePagemsg.pagesize" :pager-count="5" :page-count="collegePagemsg.pagesize" :current-page="collegePagemsg.page" :total="collegePagemsg.total" @current-change="examineListChange"></el-pagination>
+          <el-pagination
+            background
+            layout="prev, pager, next"
+            :page-size="collegePagemsg.pagesize"
+            :pager-count="5"
+            :page-count="collegePagemsg.pagesize"
+            :current-page="collegePagemsg.page"
+            :total="collegePagemsg.total"
+            @current-change="examineListChange"
+          ></el-pagination>
         </div>
         <!-- 空页面 -->
-        <div class="content" v-if="collegeListData.length == 0&&!collegeLoading">
+        <div
+          class="content"
+          v-if="collegeListData.length == 0&&!collegeLoading"
+          v-loading="collegeLoading"
+        >
           <div class="noCourse" style="text-align:center;">
-            <img src="http://static-image.1911edu.com/VIP_null.png" alt="">
+            <img src="http://static-image.1911edu.com/VIP_null.png" alt>
             <h4 style="margin-top:10px">您暂未加入任何学院，快去加入吧！</h4>
           </div>
         </div>
@@ -25,30 +38,29 @@
 </template>
 
 <script>
-import { store as persistStore } from '~/lib/core/store'
-import NoMsg from '@/pages/profile/components/common/noMsg.vue'
-import List from '@/pages/profile/components/mycollege/collegeList'
+import NoMsg from "@/pages/profile/components/common/noMsg.vue";
+import List from "@/pages/profile/components/mycollege/collegeList";
 
 export default {
-  props: ['collegeListData', 'collegePagemsg', 'collegeLoading'],
+  props: ["collegeListData", "collegePagemsg", "collegeLoading"],
   components: {
-    'v-list': List,
-    'v-nomsg': NoMsg
+    "v-list": List,
+    "v-nomsg": NoMsg
   },
-  data() {
+  data () {
     return {
       noMsg: {
-        type: 'myExamine',
-        text: '加入学院后才会有认证资格呦，快去加入吧！'
+        type: "myExamine",
+        text: "加入学院后才会有认证资格呦，快去加入吧！"
       }
-    }
+    };
   },
 
   methods: {
-    examineListChange() {
-      this.$emit('examineListChange')
+    examineListChange () {
+      this.$emit("examineListChange");
     }
   },
-  mounted() {}
-}
+  mounted () { }
+};
 </script>
