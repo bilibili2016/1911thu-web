b55140b749e52994da786a7cb374c089104553fa
ZhangShuai <zhangshuai@1911thu.com>
2018-12-07 10:06:42 +0800

    扫码下载

--- components/header/HREntry.vue
+++ components/header/HREntry.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="clearfix">
     <span class="topCenter topCode" @click="addEcg">兑换码</span>
-    <span class="hrin topCenter" @click="handleLink('/other/activePages/institutional')">单位入口
+    <span class="hrin topCenter" @click="handleLink('/other/activePages/institutional')">
+      单位入口
       <i></i>
     </span>
 
     <div class="downLoad">
       <i class="phone"></i>
       <div class="downApp clearfix">
-        <i :class={iphone:iphones} class="downIcon fl"></i>
+        <qrcode :value="downloadAppURL" :options="{ size: 120 }" class="qrcode"></qrcode>
         <div class="changeType fr">
           <span>下载1911学堂APP</span>
-          <span @mouseenter="changeImg('iphone')" :class={iphone:iphones}>
-            <i></i>AppStore下载</span>
-          <span @mouseenter="changeImg('android')" :class={iphone:!iphones}>
-            <i></i>Android下载</span>
         </div>
       </div>
     </div>
@@ -26,11 +23,16 @@
 </template>
 
 <script>
+import VueQrcode from '@xkeshi/vue-qrcode'
 import { mapState, mapGetters } from 'vuex'
+import { getNetwork } from '@/lib/util/helper'
+Vue.component(VueQrcode.name, VueQrcode)
 export default {
-  data() {
+  data () {
     return {
-      iphones: true
+      downloadAppURL: '',
+      downloadAppURL_test: 'http://ceshiapi.1911edu.com/Api/v1_1/AppH5/appDownload',
+      downloadAppURL_pro: 'http://api.1911edu.com/Api/v1_1/AppH5/appDownload',
     }
   },
   computed: {
@@ -38,18 +40,14 @@ export default {
     ...mapGetters('auth', ['isAuthenticated'])
   },
   methods: {
-    handleLink(data) {
+    handleLink (data) {
       this.$emit('handleLink', data)
     },
     // 打开头部绑定课程
-    addEcg() {
+    addEcg () {
       this.$emit('addEcg')
     },
-    // 改变鼠标悬浮时的DownApp二维码
-    changeImg(what) {
-      what == 'iphone' ? (this.iphones = true) : (this.iphones = false)
-    },
-    addClass() {
+    addClass () {
       let pathName = window.location.pathname
       if (pathName == '/other/activePages/institutional') {
         document.getElementsByClassName('hrin')[0].classList.add('active')
@@ -58,8 +56,13 @@ export default {
       }
     }
   },
-  mounted() {
+  mounted () {
     this.addClass()
+    if (getNetwork()) {
+      this.downloadAppURL = this.downloadAppURL_test
+    } else {
+      this.downloadAppURL = this.downloadAppURL_pro
+    }
   },
   watch: {
     $route: 'addClass'
