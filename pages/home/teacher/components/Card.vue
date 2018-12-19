bb7ae4e5a50eed088746d8b8b0af695ffb4a5a74
liyuru <liyuru@1911Thu.com>
2018-12-19 18:33:28 +0800

    导师招募

--- pages/home/teacher/components/Card.vue
+++ pages/home/teacher/components/Card.vue
@@ -2,26 +2,69 @@
   <div>
     <!-- 老师页 -->
     <div class="teacherCard clearfix">
-      <div class="teacherList clearfix" v-for="(teacher,index) in famousList " :key="index" @click="handleLinkTeacherInfo(teacher)">
+      <div
+        class="teacherList clearfix"
+        v-for="(teacher,index) in famousList "
+        :key="index"
+      >
         <div class="list-con">
-          <img :src="teacher.head_img" alt="" class="teacherImg">
+          <img
+            :src="teacher.head_img"
+            alt=""
+            class="teacherImg"
+          >
           <p class="name">{{teacher.teacher_name}}</p>
           <p class="position">{{teacher.graduate}}</p>
           <p class="operate">
-            <span class="btn" v-if="teacher.is_teachering" @click.stop="handleLinkTeacherInfo(teacher)">已开课</span>
-            <span class="btn preparation" v-else>筹备中</span>
-            <span class="btn" @click.stop="reservation(teacher)">预约导师</span>
+            <span
+              class="btn"
+              v-if="teacher.is_teachering"
+              @click.stop="handleLinkTeacherInfo(teacher)"
+            >已开课</span>
+            <span
+              class="btn preparation"
+              v-else
+            >筹备中</span>
+
+            <span
+              class="btn"
+              v-if="teacher.is_meet"
+              @click.stop="reservation(teacher)"
+            >预约导师</span>
+            <span
+              v-else
+              class="btn unBook"
+            >预约导师</span>
           </p>
         </div>
         <div class="mask">
-          <div class="mask-con">
+          <div
+            class="mask-con"
+            @click.stop="handleLinkTeacherInfo(teacher)"
+          >
             <h4>导师介绍</h4>
             <p>{{teacher.content}}</p>
           </div>
           <p class="operate">
-            <span class="btn" v-if="teacher.is_teachering" @click.stop="handleLinkTeacherInfo(teacher)">已开课</span>
-            <span class="btn preparation" v-else>筹备中</span>
-            <span class="btn" @click.stop="reservation(teacher)">预约导师</span>
+            <span
+              class="btn"
+              v-if="teacher.is_teachering"
+              @click.stop="handleLinkTeacherInfo(teacher)"
+            >已开课</span>
+            <span
+              class="btn preparation"
+              v-else
+            >筹备中</span>
+            <span
+              class="btn"
+              v-if="teacher.is_meet"
+              @click.stop="reservation(teacher)"
+            >预约导师</span>
+            <span
+              v-else
+              class="btn unBook"
+            >预约导师</span>
+
           </p>
         </div>
       </div>
@@ -47,35 +90,35 @@
 </template>
 
 <script>
-import { message } from '~/lib/util/helper'
-import { list } from '~/lib/v1_sdk/index'
+import { message } from "~/lib/util/helper";
+import { list } from "~/lib/v1_sdk/index";
 export default {
-  props: ['famousList'],
+  props: ["famousList"],
   data() {
     return {
       teacher: {
-        tid: ''
+        tid: ""
       }
-    }
+    };
   },
   methods: {
     reservation(teacher) {
       this.$router.push({
-        path: '/home/teacher/orderTeacher',
+        path: "/home/teacher/orderTeacher",
         query: {
           id: teacher.id
         }
-      })
+      });
     },
     handleLinkTeacherInfo(item) {
       if (item.is_teachering) {
-        this.$router.push('/home/teacher/' + item.id)
+        this.$router.push("/home/teacher/" + item.id);
       }
     }
   }
-}
+};
 </script>
 <style scoped lang="scss">
-@import '~assets/style/teacher/card';
+@import "~assets/style/teacher/card";
 </style>
 
