ff787cb7e84241e166fc1dc0a87bccd2b96506e7
liyuru <liyuru@1911Thu.com>
2018-12-24 10:11:31 +0800

    update

--- pages/shop/affirmorder/orderInfo.vue
+++ pages/shop/affirmorder/orderInfo.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <div class="orderInfo" v-if="course">
+    <div
+      class="orderInfo"
+      v-if="course"
+    >
 
       <div v-if="type!=2">
         <p>
@@ -17,7 +20,10 @@
         </p>
         <p>
           <span class="left">购买账号：</span>
-          <span class="right" :title="data.nickName">{{data.nickName}}</span>
+          <span
+            class="right"
+            :title="data.nickName"
+          >{{data.nickName}}</span>
         </p>
       </div>
       <!-- vip需要计算商品总金额 -->
@@ -32,16 +38,21 @@
         </p>
         <p>
           <span class="left">申请人昵称：</span>
-          <span class="right" :title="data.nickName">{{data.nickName}}</span>
+          <span
+            class="right"
+            :title="data.nickName"
+          >{{data.nickName}}</span>
         </p>
       </div>
 
     </div>
     <!-- 项目确认订单 -->
-    <div class="orderInfo" v-if="!course">
-      <p>
-        <span class="left" style="width:auto;">商品总金额：</span>
-        <span class="right" style="width:85px;">￥{{data.present_price}}</span>
+    <div
+      class="orderInfo"
+      v-if="!course"
+    >
+      <p class="goodsTotal">
+        商品总金额：￥{{data.present_price}}
       </p>
       <!-- <p>
         <span class="left" style="width:auto;">{{9.5}}折</span>
@@ -53,34 +64,34 @@
 </template>
 
 <script>
-import { message, matchSplits } from '@/lib/util/helper'
+import { message, matchSplits } from "@/lib/util/helper";
 export default {
-  props: ['data'],
+  props: ["data"],
   data() {
     return {
       course: true,
-      type: '',
-      id: ''
-    }
+      type: "",
+      id: ""
+    };
   },
   mounted() {
     // id: -1是课程，其他是项目和会员
-    if (matchSplits('id') == -1) {
-      this.course = true
+    if (matchSplits("id") == -1) {
+      this.course = true;
     } else {
       // type：1是项目，2是会员
-      if (matchSplits('type') == 1) {
-        this.type = matchSplits('type')
-        this.course = false
+      if (matchSplits("type") == 1) {
+        this.type = matchSplits("type");
+        this.course = false;
       } else {
-        this.type = matchSplits('type')
-        this.course = true
+        this.type = matchSplits("type");
+        this.course = true;
       }
     }
   }
-}
+};
 </script>
 
 <style scoped lang="scss">
-@import '~assets/style/shop/orderInfo.scss';
+@import "~assets/style/shop/orderInfo.scss";
 </style>
