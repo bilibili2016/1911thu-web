4abb3ca1a24fd999fdae4d632c3f2b58b042eb48
liyuru <liyuru@1911Thu.com>
2019-01-17 11:55:27 +0800

    学院介绍页滚动条问题

--- pages/home/vip/vipPage.vue
+++ pages/home/vip/vipPage.vue
@@ -4,7 +4,11 @@
       class="vipBanner"
       :class="{netWork:vipDetailData.id=='2',online:vipDetailData.id=='3'}"
     ></div>
-    <div class="con-detail">
+    <div
+      class="con-detail"
+      v-for="(collegeCon,index) in collegeArr"
+      :key="index"
+    >
       <div class="con-one clearfix">
         <div class="oneDIV clearfix">
           <div class="one-left">
@@ -183,135 +187,113 @@ export default {
         id: ""
       },
       vipInfo: "",
-      collegeCon: {
-        conOne: {
-          right: "",
-          bottom: ""
-        },
-        conTwo: {
-          twoText: "",
-          twoDesc: {
-            title: "",
-            item1: [],
-            item2: []
-          }
-        },
-        conThree: {
-          imgItems: []
-        },
-        conFour: {
-          img: ""
-        },
-        conFive: {
-          priceOne: "",
-          priceTwo: "",
-          priceThree: "",
-          priceFour: "",
-          priceFive: ""
-        }
-      },
-      networkCon: {
-        title: "在线干部学院",
-        conOne: {
-          right:
-            "1911学堂在线干部学院根据党政机关及事业单位的需求，携手清华大学、中央党校（国家行政学院）等多所全球知名院校的顶级专家和资深教授，以及政策制定和政策研究智囊专家，有针对性的研发系统化的课程体系，为各级党政机关与事业单位干部提供全球最优质的数字化课程资源，为学员带来权威、前沿、高端的学习体验。",
-          bottom: "各级各类党政机关与事业单位干部"
-        },
-        conTwo: {
-          twoText:
-            "1911学堂在线干部学院的培养目标是培养新时代忠诚担当、底蕴深厚的高素质复合型干部。使之知识储备又博又专，做到既懂专业又懂管理，既能科学决策又能统筹协调，既能把握大局又能勇于开拓。",
-          twoDesc: {
-            title: "课程特色及学员收获",
-            item1: [
-              "教研团队 阵容强大",
-              "权威师资 顶尖专家",
-              "精品课程 量身打造",
-              "在线学习 自主安排",
-              "教学一体 高效互动",
-              "清华认证 权威性高"
-            ],
-            item2: [
-              "夯实党政管理的专业知识",
-              "提高果断科学的决策能力",
-              "增强统筹协调的组织才能",
-              "树立把握全局的战略思维",
-              "增加勇于开拓的创新精神",
-              "提升党政管理的人文素养"
+      collegeArr: [],
+      networkCon: [
+        {
+          title: "在线干部学院",
+          conOne: {
+            right:
+              "1911学堂在线干部学院根据党政机关及事业单位的需求，携手清华大学、中央党校（国家行政学院）等多所全球知名院校的顶级专家和资深教授，以及政策制定和政策研究智囊专家，有针对性的研发系统化的课程体系，为各级党政机关与事业单位干部提供全球最优质的数字化课程资源，为学员带来权威、前沿、高端的学习体验。",
+            bottom: "各级各类党政机关与事业单位干部"
+          },
+          conTwo: {
+            twoText:
+              "1911学堂在线干部学院的培养目标是培养新时代忠诚担当、底蕴深厚的高素质复合型干部。使之知识储备又博又专，做到既懂专业又懂管理，既能科学决策又能统筹协调，既能把握大局又能勇于开拓。",
+            twoDesc: {
+              title: "课程特色及学员收获",
+              item1: [
+                "教研团队 阵容强大",
+                "权威师资 顶尖专家",
+                "精品课程 量身打造",
+                "在线学习 自主安排",
+                "教学一体 高效互动",
+                "清华认证 权威性高"
+              ],
+              item2: [
+                "夯实党政管理的专业知识",
+                "提高果断科学的决策能力",
+                "增强统筹协调的组织才能",
+                "树立把握全局的战略思维",
+                "增加勇于开拓的创新精神",
+                "提升党政管理的人文素养"
+              ]
+            }
+          },
+          conThree: {
+            imgItems: [
+              "http://static-image.1911edu.com/college-QH.png",
+              "http://static-image.1911edu.com/college-BD.png",
+              "http://static-image.1911edu.com/college-ZGZ.png",
+              "http://static-image.1911edu.com/college-ZKY.png",
+              "http://static-image.1911edu.com/college-ZSK.png",
+              "http://static-image.1911edu.com/college-RD.png"
             ]
+          },
+          conFour: {
+            img: "http://static-image.1911edu.com/college-pic16.png"
+          },
+          conFive: {
+            priceOne: 1800,
+            priceTwo: 78,
+            priceThree: 147,
+            priceFour: 1800,
+            priceFive: 5880
           }
-        },
-        conThree: {
-          imgItems: [
-            "http://static-image.1911edu.com/college-QH.png",
-            "http://static-image.1911edu.com/college-BD.png",
-            "http://static-image.1911edu.com/college-ZGZ.png",
-            "http://static-image.1911edu.com/college-ZKY.png",
-            "http://static-image.1911edu.com/college-ZSK.png",
-            "http://static-image.1911edu.com/college-RD.png"
-          ]
-        },
-        conFour: {
-          img: "http://static-image.1911edu.com/college-pic16.png"
-        },
-        conFive: {
-          priceOne: 1800,
-          priceTwo: 78,
-          priceThree: 147,
-          priceFour: 1800,
-          priceFive: 5880
         }
-      },
-      onlineCon: {
-        title: "在线商学院",
-        conOne: {
-          right:
-            "1911学堂在线商学院根据企业发展的需求，以解决企业组织的实际管理问题为导向，携手近百位来自清华大学、北京大学、国家发改委、国资委等权威机构的专家学者及智囊团队，整合他们在经济、金融和企业管理等领域深厚的理论功底以及各行业领域丰富的实践经验，有针对性的研发具有实用性的课程体系，为学员提供全球最优质的数字化课程资源，带来权威、前沿、高端的学习体验，助力企业中高层管理人员实现个性化、阶梯式的成长。",
-          bottom: "各级各类企业中高层管理人员"
-        },
-        conTwo: {
-          twoText:
-            "1911学堂在线商学院的培养目标是培养具备国际视野、勇于创新、能够适应客观环境变化的学习型企业经营管理者。使之具备完善的知识结构和岗位胜任能力，具备解决企业实际经营管理问题的能力，能够经得起风浪考验，适应新时代中国特色社会主义市场经济，推进供给侧结构性改革，实现企业的高质量、高效益发展。",
-          twoDesc: {
-            title: "课程特色及学员收获",
-            item1: [
-              "教研团队 阵容强大",
-              "权威师资 顶尖专家",
-              "精品课程 量身打造",
-              "在线学习 自主安排",
-              "教学一体 高效互动",
-              "清华认证 权威性高"
-            ],
-            item2: [
-              "夯实党政管理的专业知识",
-              "提高果断科学的决策能力",
-              "增强统筹协调的组织才能",
-              "树立把握全局的战略思维",
-              "增加勇于开拓的创新精神",
-              "提升企业领导的人文素养"
+      ],
+      onlineCon: [
+        {
+          title: "在线商学院",
+          conOne: {
+            right:
+              "1911学堂在线商学院根据企业发展的需求，以解决企业组织的实际管理问题为导向，携手近百位来自清华大学、北京大学、国家发改委、国资委等权威机构的专家学者及智囊团队，整合他们在经济、金融和企业管理等领域深厚的理论功底以及各行业领域丰富的实践经验，有针对性的研发具有实用性的课程体系，为学员提供全球最优质的数字化课程资源，带来权威、前沿、高端的学习体验，助力企业中高层管理人员实现个性化、阶梯式的成长。",
+            bottom: "各级各类企业中高层管理人员"
+          },
+          conTwo: {
+            twoText:
+              "1911学堂在线商学院的培养目标是培养具备国际视野、勇于创新、能够适应客观环境变化的学习型企业经营管理者。使之具备完善的知识结构和岗位胜任能力，具备解决企业实际经营管理问题的能力，能够经得起风浪考验，适应新时代中国特色社会主义市场经济，推进供给侧结构性改革，实现企业的高质量、高效益发展。",
+            twoDesc: {
+              title: "课程特色及学员收获",
+              item1: [
+                "教研团队 阵容强大",
+                "权威师资 顶尖专家",
+                "精品课程 量身打造",
+                "在线学习 自主安排",
+                "教学一体 高效互动",
+                "清华认证 权威性高"
+              ],
+              item2: [
+                "夯实党政管理的专业知识",
+                "提高果断科学的决策能力",
+                "增强统筹协调的组织才能",
+                "树立把握全局的战略思维",
+                "增加勇于开拓的创新精神",
+                "提升企业领导的人文素养"
+              ]
+            }
+          },
+          conThree: {
+            imgItems: [
+              "http://static-image.1911edu.com/college-QH.png",
+              "http://static-image.1911edu.com/college-BD.png",
+              "http://static-image.1911edu.com/college-GFG.png",
+              "http://static-image.1911edu.com/college-ZRK.png",
+              "http://static-image.1911edu.com/college-ZKY.png",
+              "http://static-image.1911edu.com/college-ZZF.png"
             ]
+          },
+          conFour: {
+            img: "http://static-image.1911edu.com/college-pic14.png"
+          },
+          conFive: {
+            priceOne: 3600,
+            priceTwo: 63,
+            priceThree: 187,
+            priceFour: 3600,
+            priceFive: 11220
           }
-        },
-        conThree: {
-          imgItems: [
-            "http://static-image.1911edu.com/college-QH.png",
-            "http://static-image.1911edu.com/college-BD.png",
-            "http://static-image.1911edu.com/college-GFG.png",
-            "http://static-image.1911edu.com/college-ZRK.png",
-            "http://static-image.1911edu.com/college-ZKY.png",
-            "http://static-image.1911edu.com/college-ZZF.png"
-          ]
-        },
-        conFour: {
-          img: "http://static-image.1911edu.com/college-pic14.png"
-        },
-        conFive: {
-          priceOne: 3600,
-          priceTwo: 63,
-          priceThree: 187,
-          priceFour: 3600,
-          priceFive: 11220
         }
-      }
+      ]
     };
   },
   components: {
@@ -382,9 +364,9 @@ export default {
       this.relativeID = matchSplits("cid");
       this.vipDetailData.id = matchSplits("id"); //2:干部网络学院  3:在线商学院
       if (this.vipDetailData.id == "2") {
-        this.collegeCon = Object.assign({}, this.networkCon);
+        this.collegeArr = this.networkCon;
       } else {
-        this.collegeCon = Object.assign({}, this.onlineCon);
+        this.collegeArr = this.onlineCon;
       }
     }
   },
