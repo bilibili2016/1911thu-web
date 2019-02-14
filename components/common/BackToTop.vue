436e93ca4e853f7e1847777cd79d3062a8df33db
ZhangShuai <zhangshuai@1911thu.com>
2019-02-14 18:49:55 +0800

    update

--- components/common/BackToTop.vue
+++ components/common/BackToTop.vue
@@ -10,9 +10,9 @@
       <div class="back-to-ceiling hasColor customStyleOne" v-show="visible">
         <span></span>
         <div class="showCode">
-          <img src="http://static-image.1911edu.com/attentionWechat3.jpeg" alt="">
+          <img src="http://static-image.1911edu.com/attentionWechat2.jpg" alt="">
           <h4>1911学堂官方微信</h4>
-          <p>微信号：XUETANG_1911</p>
+          <p>公众号：XUETANG_1911</p>
         </div>
       </div>
     </transition>
