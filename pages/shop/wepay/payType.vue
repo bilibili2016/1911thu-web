f7fad76e1c00b4fb41a4018cec05c015f4211aac
liyuru <liyuru@1911Thu.com>
2019-03-06 16:08:14 +0800

    update

--- pages/shop/wepay/payType.vue
+++ pages/shop/wepay/payType.vue
@@ -34,27 +34,26 @@
     <div class="fk fr" @click="showRpt">
       我有疑问，需要反馈?
     </div>
-
   </div>
 </template>
 
 <script>
-import { store as persistStore } from '~/lib/core/store'
-import { timestampToTime, matchSplits } from '~/lib/util/helper'
-import { wepay } from '@/lib/v1_sdk/index'
+import { store as persistStore } from "~/lib/core/store";
+import { timestampToTime, matchSplits } from "~/lib/util/helper";
+import { wepay } from "@/lib/v1_sdk/index";
 export default {
-  props: ['orderDetail', 'codeData', 'listData'],
+  props: ["orderDetail", "codeData", "listData"],
   data() {
     return {
-      wechatPay: 'http://static-image.1911edu.com/wxp.png',
-      zfbPay: 'http://static-image.1911edu.com/zfb.png',
-      orderType: '',
+      wechatPay: "http://static-image.1911edu.com/wxp.png",
+      zfbPay: "http://static-image.1911edu.com/zfb.png",
+      orderType: "",
       wxMsg: true,
       zfbMsg: false,
       pubMsg: false,
-      val: '',
+      val: "",
       addPaySubmitForm: {
-        types: '1',
+        types: "1",
         companyId: null
       },
       payListForm: {
@@ -62,73 +61,79 @@ export default {
         attachs: 1
       },
       projectList: [],
-      restTime: '',
+      restTime: "",
       countdown: {
-        hour: '',
-        minutes: '',
-        seconds: ''
+        hour: "",
+        minutes: "",
+        seconds: ""
       },
       isWSW: false
       // attachs: null
-    }
+    };
   },
   watch: {
     orderDetail(val) {
       if (val.order_amount > 5000 && val.order_amount < 500000) {
         this.$alert(
-          '您的订单支付金额超过5000元，建议您通过对公转账方式进行支付，感谢配合！',
-          '温馨提示',
+          "您的订单支付金额超过5000元，建议您通过对公转账方式进行支付，感谢配合！",
+          "温馨提示",
           {
-            confirmButtonText: '确定',
+            closeOnHashChange: true,
+            confirmButtonText: "确定",
+            beforeClose: (action, instance, done) => {
+              console.log(action, "kkk");
+
+              done(); // 就是它!
+            },
             callback: action => {}
           }
-        )
-        this.wxMsg = false
-        this.zfbMsg = false
-        this.pubMsg = true
-        this.wechatPay = 'http://static-image.1911edu.com/wxp.png'
-        this.zfbPay = 'http://static-image.1911edu.com/zfb.png'
+        );
+        this.wxMsg = false;
+        this.zfbMsg = false;
+        this.pubMsg = true;
+        this.wechatPay = "http://static-image.1911edu.com/wxp.png";
+        this.zfbPay = "http://static-image.1911edu.com/zfb.png";
       } else if (val.order_amount >= 500000) {
         this.$alert(
-          '您的订单支付金额超过50万元，请通过对公转账方式进行支付，感谢配合！',
-          '温馨提示',
+          "您的订单支付金额超过50万元，请通过对公转账方式进行支付，感谢配合！",
+          "温馨提示",
           {
-            confirmButtonText: '确定',
+            confirmButtonText: "确定",
             callback: action => {}
           }
-        )
-        this.selectPub()
-        this.isWSW = true
-        this.wechatPay = 'http://static-image.1911edu.com/wxpUnclick.png'
-        this.zfbPay = 'http://static-image.1911edu.com/zfbUnclick.png'
+        );
+        this.selectPub();
+        this.isWSW = true;
+        this.wechatPay = "http://static-image.1911edu.com/wxpUnclick.png";
+        this.zfbPay = "http://static-image.1911edu.com/zfbUnclick.png";
       } else {
-        this.wechatPay = 'http://static-image.1911edu.com/wxp.png'
-        this.zfbPay = 'http://static-image.1911edu.com/zfb.png'
-        this.isWSW = false
+        this.wechatPay = "http://static-image.1911edu.com/wxp.png";
+        this.zfbPay = "http://static-image.1911edu.com/zfb.png";
+        this.isWSW = false;
       }
     }
   },
   methods: {
     showRpt() {
-      this.$emit('showRpt')
+      this.$emit("showRpt");
     },
     addPaySubmit() {
-      this.wxMask = true
+      this.wxMask = true;
       if (this.wxMsg === true) {
-        this.$bus.$emit('showCode', this.codeData.code_url)
-        this.getStatus()
+        this.$bus.$emit("showCode", this.codeData.code_url);
+        this.getStatus();
       } else if (this.zfbMsg == true) {
-        this.$bus.$emit('showCode', this.codeData.qr_code)
-        this.getStatus()
+        this.$bus.$emit("showCode", this.codeData.qr_code);
+        this.getStatus();
       } else if (this.pubMsg == true) {
-        let urlLen = matchSplits('order')
-        this.$router.push('/shop/payPublic?orderID=' + urlLen)
+        let urlLen = matchSplits("order");
+        this.$router.push("/shop/payPublic?orderID=" + urlLen);
       }
     },
     getStatus() {
-      let cpyid = matchSplits('order')
+      let cpyid = matchSplits("order");
       // let attachs = matchSplits('attach')
-      this.payListForm.orderId = cpyid
+      this.payListForm.orderId = cpyid;
       // this.payListForm.attachs = 2
       // if (attachs == 2) {
       //   this.payListForm.attachs = 2
@@ -136,70 +141,70 @@ export default {
       // }
       this.interval = setInterval(() => {
         if (this.seconds <= 0) {
-          clearInterval(this.interval)
+          clearInterval(this.interval);
         } else {
-          this.seconds--
+          this.seconds--;
           wepay.payResult(this.payListForm).then(response => {
             if (response.status === 0) {
-              clearInterval(this.interval)
-              this.$bus.$emit('closeCode')
+              clearInterval(this.interval);
+              this.$bus.$emit("closeCode");
               this.$router.push({
-                path: '/shop/payResult' + '?order=' + cpyid
-              })
+                path: "/shop/payResult" + "?order=" + cpyid
+              });
             } else {
               if (response.status == 100007) {
-                this.$router.push('/')
-                this.$bus.$emit('loginShow', true)
+                this.$router.push("/");
+                this.$bus.$emit("loginShow", true);
               }
             }
-          })
+          });
         }
-      }, 1000)
+      }, 1000);
     },
     selectWx() {
       if (!this.isWSW) {
-        this.wxMsg = true
-        this.zfbMsg = false
-        this.pubMsg = false
+        this.wxMsg = true;
+        this.zfbMsg = false;
+        this.pubMsg = false;
       }
     },
     selectZfb() {
       if (!this.isWSW) {
-        this.wxMsg = false
-        this.zfbMsg = true
-        this.pubMsg = false
+        this.wxMsg = false;
+        this.zfbMsg = true;
+        this.pubMsg = false;
       }
     },
     selectPub() {
-      this.wxMsg = false
-      this.zfbMsg = false
-      this.pubMsg = true
+      this.wxMsg = false;
+      this.zfbMsg = false;
+      this.pubMsg = true;
     },
     time() {
       setInterval(data => {
         if (this.orderDetail.create_time) {
-          let mss = new Date() - new Date(this.orderDetail.create_time * 1000)
-          this.MillisecondToDate(mss)
+          let mss = new Date() - new Date(this.orderDetail.create_time * 1000);
+          this.MillisecondToDate(mss);
         }
-      }, 1000)
+      }, 1000);
     },
     MillisecondToDate(msd) {
-      var time = 24 * 60 * 60 - parseFloat(msd) / 1000
-      if (null != time && '' != time) {
+      var time = 24 * 60 * 60 - parseFloat(msd) / 1000;
+      if (null != time && "" != time) {
         if (time > 60 && time < 60 * 60) {
           time =
             parseInt(time / 60.0) +
-            '分钟' +
+            "分钟" +
             parseInt((parseFloat(time / 60.0) - parseInt(time / 60.0)) * 60) +
-            '秒'
+            "秒";
         } else if (time >= 60 * 60 && time < 60 * 60 * 24) {
           time =
             parseInt(time / 3600.0) +
-            '小时' +
+            "小时" +
             parseInt(
               (parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60
             ) +
-            '分钟' +
+            "分钟" +
             parseInt(
               (parseFloat(
                 (parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60
@@ -209,27 +214,27 @@ export default {
                 )) *
                 60
             ) +
-            '秒'
+            "秒";
         } else {
-          time = '订单已过期！'
+          time = "订单已过期！";
         }
       }
-      this.restTime = time
+      this.restTime = time;
     }
   },
   mounted() {
     //this.orderType 1:其他订单 2:vip订单
-    this.orderType = matchSplits('type')
-    this.$bus.$on('clearInterval', dat => {
-      clearInterval(this.interval)
-    })
-    this.$bus.$on('addPaySubmit', dat => {
-      this.addPaySubmit()
-    })
+    this.orderType = matchSplits("type");
+    this.$bus.$on("clearInterval", dat => {
+      clearInterval(this.interval);
+    });
+    this.$bus.$on("addPaySubmit", dat => {
+      this.addPaySubmit();
+    });
     // this.time()
   }
-}
+};
 </script>
 <style lang="scss" scoped>
-@import '~assets/style/shop/paytype.scss';
+@import "~assets/style/shop/paytype.scss";
 </style>
