bd8561cf06123c4d384a80f56627c118e40fd61b
ZhangShuai <zhangshuai@1911thu.com>
2019-03-12 13:41:40 +0800

    学位项目、测评体系

---
+++ pages/other/pages/system.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="noCollege">
+    <img src="http://static-image.1911edu.com/noCollege.png" alt="">
+    <p>正在筹备中，敬请期待...</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+@import "~assets/style/other/system";
+</style>
