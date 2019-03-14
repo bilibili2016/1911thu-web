d399281656a64b564e7c8eac594ae993efe6fbeb
liyuru <liyuru@1911Thu.com>
2019-03-14 17:04:32 +0800

    update

--- pages/home/vip/components/BusinessCollege.vue
+++ pages/home/vip/components/BusinessCollege.vue
@@ -1,13 +1,13 @@
 <template>
   <!-- 商学院 -->
   <div class="imgList">
-    <img class="collegeImg" v-for="(url,index) in data" :key="index" :src="url" alt="">
+    <!-- <img class="collegeImg" v-for="(url,index) in data" :key="index" :src="url" alt=""> -->
   </div>
 </template>
 
 <script>
 export default {
-  props: ["data"]
+  // props: ["data"]
 };
 </script>
 
