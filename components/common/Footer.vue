c86a479e025db6d2c0334cd54f9d176ab2ecb9b7
ZhangShuai <zhangshuai@1911thu.com>
2019-03-15 09:56:53 +0800

    添加资质

--- components/common/Footer.vue
+++ components/common/Footer.vue
@@ -11,7 +11,8 @@
         <p class="one">一九一一未来教育科技（北京）有限公司 版权所有</p>
         <p class="two">京ICP备18040329号&nbsp; | &nbsp;
           <span @click="handleClickA" style="cursor:pointer">京公网安备 11010802028321号</span>&nbsp; | &nbsp;
-          <span @click="handleClickB" style="cursor:pointer">广播电视节目制作经营许可证(京)字第11716号</span>
+          <span @click="handleClickB" style="cursor:pointer">广播电视节目制作经营许可证(京)字第11716号</span>&nbsp; | &nbsp;
+          <span @click="handleClickC" style="cursor:pointer">增值电信业务经营许可证京B2-20190479</span>
         </p>
       </div>
     </div>
@@ -30,6 +31,9 @@ export default {
     },
     handleClickA () {
       window.open('http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11010802028321')
+    },
+    handleClickC () {
+      window.open('http://static-image.1911edu.com/qualification.jpg')
     }
   }
 };
