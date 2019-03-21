573e546c6320c53b73e407956968f6e114ffdfe7
ZhangShuai <zhangshuai@1911thu.com>
2019-03-21 17:03:46 +0800

    学院重构

---
+++ pages/home/vip/pages/CommercialCollege.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- 商学院 -->
+  <div class="imgList">
+    <img class="collegeImg" v-for="(url,index) in data" :key="index" :src="url" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["data"]
+};
+</script>
+
+<style scoped>
+</style>
