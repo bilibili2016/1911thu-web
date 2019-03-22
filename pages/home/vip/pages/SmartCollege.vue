d44334195b1ffb10d0afca151c28e6f92b6f04a3
ZhangShuai <zhangshuai@1911thu.com>
2019-03-22 10:26:37 +0800

    在线干部学院，人工智能学院静态页

--- pages/home/vip/pages/SmartCollege.vue
+++ pages/home/vip/pages/SmartCollege.vue
@@ -1,14 +1,115 @@
 <template>
-  <div class="imgList">
-    <img class="collegeImg" v-for="(url,index) in data" :key="index" :src="url" alt="">
+  <div class="smartCollege">
+    <div class="vipBanner"></div>
+    <div class="container">
+      <div class="intro">
+        <p>据全球性信息分析公司爱思唯尔的最新数据显示：在全球范围内，人工智能研究在过去5年以每年接近13％的速度快速增长。国际数据公司 (IDC) 则预计，到2022年，中国人工智能市场规模将达到98.4亿美元。</p>
+        <p>随着人工智能技术的快速发展与市场规模急剧扩大，我国人工智能人才开始显现供不应求的局面。据业内人士估算，我国人工智能领域的人才缺口数量在百万级别。</p>
+        <p>在此背景下，1911学堂在线人工智能学院面向各单位相关岗位人员提供系统性的教育培训解决方案，通过优质教育资源整合及产学研协同，培养人工智能领域的复合型人才，特别是贯通人工智能理论、方法、技术、产品与应用等的纵向复合型人才，以及掌握 “人工智能+” 经济、社会、管理、标准、法律等的横向复合型人才。</p>
+        <p>1911学堂与清华大学相关部门及专家教授成立横向课题，建立了在线人工智能学院专家组，共同开展学术课题、课程研发及认证合作。学院专家组对在线人工智能学院的课程体系、授课师资、培养目标、技术平台、学习模式、测评体系、考试系统等进行了充分论证与整体评估，并出具了课程认证与鉴定意见。</p>
+      </div>
+      <div class="studyInfo">
+        <div class="main">
+          <h2 class="first">专家组组长</h2>
+          <div class="group clearfix">
+            <img class="fl" src="http://static-image.1911edu.com/smartCollege_teacher.png" alt="">
+            <div class="fr">
+              <h4>孙富春 教授</h4>
+              <p>清华大学计算机科学与技术系教授、博士生导师，校科研院院长助理，国家863计划专家组成员，兼任大连海事大学讲座教授、博士生导师，国际刊物《IEEE Trans. On Neural Networks》《Soft Computing》和《International Journal of Computational Intelligence Systems》编委，IEEE控制系统学会国际会议编委会编委，IEEE控制系统学会智能控制技术委员会委员。在清华大学计算机应用专业获博士学位，并于清华大学自动化系从事博士后研究，后在计算机科学与技术系工作至今。</p>
+            </div>
+
+          </div>
+        </div>
+        <div class="leftBg">
+          <h2 class="margin">面向人群</h2>
+          <div class="object">
+            <p class="one">人工智能产业企业家、经营管理人员、技术人员</p>
+            <p class="two">转型升级的传统企业经营管理人员</p>
+            <p class="three">各地经信化、科技等相关部门管理人员</p>
+            <p class="four">人工智能研究人员</p>
+            <p class="five">对人工智能领域感兴趣的人士</p>
+          </div>
+          <h2>学习内容</h2>
+          <ul class="study">
+            <li>
+              <span>1</span>
+              <span>人工智能的发展历史现状和发展趋势</span>
+            </li>
+            <li>
+              <span>6</span>
+              <span>人工智能AI原理和核心技术解读</span>
+            </li>
+            <li>
+              <span>2</span>
+              <span>人工智能时代的产业趋势与商业机会</span>
+            </li>
+            <li>
+              <span>7</span>
+              <span>利用人工智能提升公共安全保障能力</span>
+            </li>
+            <li>
+              <span>3</span>
+              <span>人工智能在教育中的应用</span>
+            </li>
+            <li>
+              <span>8</span>
+              <span>人工智能领域军民创新资源共建共享</span>
+            </li>
+            <li>
+              <span>4</span>
+              <span>利用人工智能技术实现制造业转型升级</span>
+            </li>
+            <li>
+              <span>9</span>
+              <span>建设安全便捷的智能社会</span>
+            </li>
+            <li>
+              <span>5</span>
+              <span>大数据下的人工智能</span>
+            </li>
+            <li>
+              <span>10</span>
+              <span>人工智能对物流、金融、税务等行业的影响和挑战</span>
+            </li>
+          </ul>
+        </div>
+        <div class="bottomBg">
+          <div class="columns clearfix">
+            <p>1911学堂在线人工智能学院的课程内容注重理论联系实际，教学过程中有丰富的教学设计及交互活动，能够充分满足学员的学习需求，从而有效提高学员发现问题、分析问题和解决问题的能力，有利于实现个人与社会的双重目标。</p>
+            <img src="http://static-image.1911edu.com/smartCollege_icon1.png" alt="">
+          </div>
+          <div class="harvest">
+            <h2>学习收获</h2>
+            <p>通过参加1911学堂在线人工智能学院的学习，学习者可以了解人工智能发展趋势与商业机会、提升未来职业发展能力，掌握人工智能AI原理及核心技术，了解人工智能对物流、金融、税务等行业的影响和挑战，加强人工智能基础研究、应用研究、运行维护等方面专业技术，成为贯通人工智能理论、方法、技术、产品与应用等的纵向复合型人才，以及掌握“人工智能+” 经济、社会、管理、标准、法律等的横向复合型人才。</p>
+          </div>
+        </div>
+        <div class="attestation">
+          <h2>考核认证</h2>
+          <div class="clearfix">
+            <div class="left fl">
+              <span class="circle"></span>
+              <span class="text">学员参加1911学堂在线人工智能学院学习，学习结束后可申请参加相关考试，考核通过者将获得清华大学相关部门颁发的认证证书及1911学堂结业证书。</span>
+            </div>
+            <div class="right fr">
+              <img src="http://static-image.1911edu.com/cadreCollege_icon2.png" alt>
+            </div>
+          </div>
+
+          <div class="certification">
+            <img src="http://static-image.1911edu.com/smartCollege_certificate.png" alt>
+          </div>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['data']
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@import "assets/style/home/smartCollege";
 </style>
