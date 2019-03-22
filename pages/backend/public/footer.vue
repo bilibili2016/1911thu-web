375278e1bfadad2a441784e8f915485aac47e957
ZhangShuai <zhangshuai@1911thu.com>
2019-03-22 10:25:52 +0800

    backend中的静态页头部改写

--- pages/backend/public/footer.vue
+++ pages/backend/public/footer.vue
@@ -4,14 +4,12 @@
       <div class="logo fl">
         <i></i>
       </div>
-      <div class="companyInfo fl">
-        <span class="aboutSpan">关于我们</span>
-      </div>
       <div class="copyright fr">
         <p class="one">一九一一未来教育科技（北京）有限公司 版权所有</p>
-        <p class="two">
-          京ICP备18040329号 | &nbsp;&nbsp;
-          <span>广播电视节目制作经营许可证(京)字第11716号</span>
+        <p class="two">京ICP备18040329号&nbsp; | &nbsp;
+          <span style="cursor:pointer">京公网安备 11010802028321号</span>&nbsp; | &nbsp;
+          <span style="cursor:pointer">广播电视节目制作经营许可证(京)字第11716号</span>&nbsp; | &nbsp;
+          <span style="cursor:pointer">增值电信业务经营许可证京B2-20190479</span>
         </p>
       </div>
     </div>
