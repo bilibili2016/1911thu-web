a35554c31573644bb6f78e0c64e7c2f0a5dc15f8
ZhangShuai <zhangshuai@1911thu.com>
2019-03-22 14:10:44 +0800

    在线健康学院，体育学院

--- pages/home/vip/pages/SportsCollege.vue
+++ pages/home/vip/pages/SportsCollege.vue
@@ -1,14 +1,126 @@
 <template>
-  <div class="imgList">
-    <img class="collegeImg" v-for="(url,index) in data" :key="index" :src="url" alt="">
+  <!-- 干部学院 -->
+  <div class="cadreCollege">
+    <div class="vipBanner"></div>
+    <div class="container">
+      <div class="main">
+        <div class="intro">
+          <p>国务院 “46号文” 开启了中国体育产业的黄金时代，预计到2020年，我国体育产业产值将达到3万亿，2025年将达到5万亿，然而体育产业从业人员却存在数百万级的缺口。种种现象表明，我国急需既懂体育、又懂经营管理的复合型人才。</p>
+          <p>1911学堂在线体育学院拥有众多体育领域的资深导师，面向运动员、体育系统干部、体育产业经营管理人员三类人群，提供系统性的教育培训解决方案，旨在培养具有较高专业成绩、文化水平、社会适应能力及综合素质的人才；勤政廉政、奋发有为、善于推动体育事业科学发展的高素质党政人才；熟悉体育产业工作规律，具备市场运作、经营开发和产业管理能力的体育产业人才。</p>
+          <p>1911学堂与清华大学相关部门及专家教授成立横向课题，建立了在线体育学院专家组，共同开展学术课题、课程研发及认证合作。学院专家组对在线体育学院的课程体系、授课师资、培养目标、技术平台、学习模式、测评体系、考试系统等进行了充分论证与整体评估，并出具了课程认证与鉴定意见。</p>
+        </div>
+        <h2>专家组组长</h2>
+        <div class="group">
+          <div class="professor">
+            <h4>刘波 教授</h4>
+            <p>清华大学体育部主任，本科及硕士毕业于清华大学，博士毕业于德国科隆体育大学。曾出版德文和中文专著3本；发表各类学术论文50余篇，其中30余篇发表在核心期刊；完成和在研课题10项，其中8项为课题负责人。获得2018年北京市教学成果一等奖（第一完成人）；2016年第八届中国学校体育科学大会科学论文报告会一等奖；2016年清华大学教学成果一等奖（第一完成人）等荣誉。</p>
+          </div>
+        </div>
+        <h2 class="first">面向人群</h2>
+        <div class="object">
+          <div class="one">
+            <h5>运动员</h5>
+            <img src="http://static-image.1911edu.com/sportCollege_icon8.png" alt="">
+            <p>包括国家队体育运动员，全国各省、自治区、直辖市以及地（市）、县（市）高等体育类院校运动员，全国各级体工队运动员，全国各俱乐部体育运动员。</p>
+          </div>
+          <div class="two">
+            <h5>体育系统干部</h5>
+            <img src="http://static-image.1911edu.com/sportCollege_icon9.png" alt="">
+            <p>包括各级体育局机关领导干部、体育事业单位领导干部、中青年后备干部、基层干部、体育社团组织领导等。</p>
+          </div>
+          <div class="three">
+            <h5>体育产业经营管理人员</h5>
+            <img src="http://static-image.1911edu.com/sportCollege_icon10.png" alt="">
+            <p>包括各地体育运动社团组织、体育培训服务机构、体育赛事组织、体育运动俱乐部、体育场馆、体育用品生产与销售企业、体育地产企业、体育旅游企业、体育中介企业等组织机构的经营管理者。</p>
+          </div>
+        </div>
+      </div>
+      <div class="centerBg">
+        <div class="main">
+          <h2>学习内容</h2>
+          <ul class="study">
+            <li>
+              <span>1</span>
+              <span>体育行业发展</span>
+            </li>
+            <li>
+              <span>2</span>
+              <span>体育投融资</span>
+            </li>
+            <li>
+              <span>3</span>
+              <span>赛事组织</span>
+            </li>
+            <li>
+              <span>4</span>
+              <span>场馆运营</span>
+            </li>
+            <li>
+              <span>5</span>
+              <span>体育培训</span>
+            </li>
+            <li>
+              <span>6</span>
+              <span>体育营销及IP版权</span>
+            </li>
+            <li>
+              <span>7</span>
+              <span>电竞</span>
+            </li>
+            <li>
+              <span>8</span>
+              <span>“体育+” 融合发展</span>
+            </li>
+            <li>
+              <span>9</span>
+              <span>运动与健康</span>
+            </li>
+            <li>
+              <span>10</span>
+              <span>运动员职业转型</span>
+            </li>
+            <li>
+              <span>11</span>
+              <span>体育干部领导力提升</span>
+            </li>
+          </ul>
+          <div class="columns">
+            1911学堂在线体育学院的课程内容注重理论联系实际，教学过程中有丰富的教学设计及交互活动，能够充分满足学员的学习需求，从而有效提高学员发现问题、分析问题和解决问题的能力，有利于实现个人与社会的双重目标。
+          </div>
+          <h2>学习收获</h2>
+          <div class="harvest clearfix">
+            <p class="fl">运动员可以了解到未来职业发展趋势、提升未来职业发展能力，掌握运动科学基本知识、科学预防训练损伤和伤病康复，掌握创业实务知识、了解创业基本过程，提升创业技能，成功实现职业转型升级，从行业从业者成长为成功的行业领导者。</p>
+            <p class="fr">体育系统干部与体育产业经营管理人员可以夯实体育管理的专业知识、提高果断科学的决策能力、增强统筹协调的组织才能、树立把握全局的战略思维、增加勇于开拓的创新精神、提升体育管理的人文素养。人员所在单位机构也将能够提升组织绩效，实现组织战略发展目标。</p>
+          </div>
+        </div>
+      </div>
+      <div class="main">
+        <h2>考核认证</h2>
+        <div class="attestation">
+          <div class="clearfix">
+            <div class="left fl">
+              <span class="circle"></span>
+              <span class="text">学员参加1911学堂在线体育学院学习，学习结束后可申请参加相关考试，考核通过者将获得清华大学相关部门颁发的认证证书及1911学堂结业证书。</span>
+            </div>
+            <div class="right fr">
+              <img src="http://static-image.1911edu.com/cadreCollege_icon2.png" alt>
+            </div>
+          </div>
+          <div class="certification">
+            <img src="http://static-image.1911edu.com/sportCollege_certificate.png" alt>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['data']
-}
+
+};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@import "assets/style/home/SportsCollege";
 </style>
