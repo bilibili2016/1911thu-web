2523f101d92ccdb28380595f8fd2cdb648606e6c
liyuru <liyuru@1911Thu.com>
2019-04-23 18:53:16 +0800

    update

---
+++ pages/profile/components/myteacher/inputTime.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="inputTime">
+
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+@import "~assets/style/profile/myTeacher/inputTime";
+</style>
+
---
+++ pages/profile/components/myteacher/timeTable.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="timeTable">
+
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+@import "~assets/style/profile/myTeacher/timeTable";
+</style>
+
