c64acb81eef7fde349586a0b9f0f04dbfb93e82d
ZhangShuai <zhangshuai@1911thu.com>
2019-05-07 17:58:17 +0800

    视频测试

--- pages/test/player.vue
+++ pages/test/player.vue
@@ -8,6 +8,7 @@
 
 <script>
 import { profileHome } from "~/lib/v1_sdk/index";
+import { message } from "@/lib/util/helper";
 export default {
   data () {
     return {
@@ -23,20 +24,18 @@ export default {
   methods: {
     begin () {
       // 先关闭之前的推流
-      this.aliWebrtc.leaveChannel();
-      this.aliWebrtc.dispose();
+      //   this.aliWebrtc.leaveChannel();
+      //   this.aliWebrtc.dispose();
 
       this.otherAli()
-      this.creatAliplayer()
     },
+    // 2. 找到播放器预览
     creatAliplayer () {
       this.aliWebrtc.startPreview(this.$refs.video).then((obj) => {
-        console.log(this.authInfo, this.userName, 'yes');
-        this.aliwebrtc.joinChannel(this.authInfo, this.userName).then((obj) => {
-          console.log(obj, '入会成功');
-
+        // 3. 加入房间
+        this.aliWebrtc.joinChannel(this.authInfo, this.userName).then((obj) => {
           // 入会成功
-
+          this.publishLocalStreams()
         }, (error) => {
           // 入会失败，这里console下error内容，可以看到失败原因
           console.log(error.message);
@@ -45,51 +44,24 @@ export default {
       }).catch((error) => {
         // 预览失败
       });
-      //   this.aliWebrtc.startPreview(this.$refs.video).then((obj) => {
-      //     // var localVideo = $('.local-video video');
-      //     // localVideo[0].srcObject = obj.stream;
-      //     //2. 获取频道鉴权令牌参数
-      //     getRTCAuthInfo().then((authInfo) => {
-      //       console.log(authInfo);
-
-      //       //3. 加入房间
-      //       this.aliWebrtc.joinChannel(authInfo, userName).then(() => {
-      //         console.log('加入房间成功');
-      //         // 4. 发布本地流
-      //         this.aliWebrtc.publish().then((res) => {
-      //           console.log('发布流成功');
-      //         }, (error) => {
-      //           console.log(1);
-
-      //           alert(error.message);
-      //         });
-      //       }).catch((error) => {
-      //         console.log(2);
-      //         alert(error.message);
-      //       })
-      //     }).catch((error) => {
-      //       console.log(3)
-      //       alert(error.message);
-      //     });
-      //   }).catch((error) => {
-      //     console.log(4);
-
-      //     alert(error.message); // 预览失败
-      //   });
     },
+    // 4. 发布本地流
+    publishLocalStreams () {
+      this.aliWebrtc.publish().then((res) => {
+        console.log('发布流成功');
+        message(this, "success", '发布流成功')
+      }, (error) => {
+        console.log(1);
 
+        alert(error.message);
+      });
+    },
+    // 1. 获取频道鉴权令牌参数
     otherAli () {
-      console.log(1);
-
       profileHome.otherAli(this.aliPlayer).then(res => {
-        console.log(2);
-
-        if (res.status == 0) {
-          console.log(3);
-
-          console.log(res.data, ' res.data');
-
+        if (res.code == 0) {
           this.authInfo = res.data
+          this.creatAliplayer()
         } else {
           message(this, "error", res.msg);
         }
